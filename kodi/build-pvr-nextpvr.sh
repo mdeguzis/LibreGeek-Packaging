ffa3aac30759dc42eb6ceef2c6c7fd01888f0e94
mikeyd <mdeguzis@gmail.com>
2015-11-15 19:58:37 -0500

    update with proper version

--- kodi/build-pvr-nextpvr.sh
+++ kodi/build-pvr-nextpvr.sh
@@ -3,7 +3,7 @@
 # Author:	Michael DeGuzis
 # Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	build-pvr-nextpvr.sh
-# Script Ver:	0.1.1
+# Script Ver:	1.0.0
 # Description:	Attempts to build a deb package from Kodi PVR nextpvr addon git source
 #
 # See:		https://github.com/kodi-pvr/pvr.nextpvr
@@ -24,7 +24,8 @@ git_branch="Isengard"
 date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
 date_short=$(date +%Y%m%d)
 pkgname="kodi-pvr-nextpvr"
-pkgver="${date_short}+git"
+#pkgver="${date_short}+git"
+pkgver="1.9.27+git"
 pkgrev="1"
 dist_rel="brewmaster"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"

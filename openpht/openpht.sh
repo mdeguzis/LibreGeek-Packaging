bad99e590f5e09dda960c4f7af649ff304e92498
mikeyd <mdeguzis@gmail.com>
2015-12-07 11:16:19 -0500

    correct for openpht rebrand

--- openpht/openpht.sh
+++ openpht/openpht.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export XBMC_HOME=/opt/plexhometheater/share/XBMC
+export XBMC_HOME=/opt/openpht/share/XBMC
 #Use export AE_ENGINE=SOFT to disable pulse audio
 #export AE_ENGINE=SOFT
-/opt/plexhometheater/bin/plexhometheater
+/opt/openpht/bin/plexhometheater

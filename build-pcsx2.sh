ad4f13fd3836a60f874dad2407f9c7e89f590bfb
mikeyd <mdeguzis@gmail.com>
2015-12-09 11:46:58 -0500

    Update build-pcsx2.sh

--- build-pcsx2.sh
+++ build-pcsx2.sh
@@ -1,61 +1,59 @@
 #!/bin/bash
-
-# -------------------------------------------------------------------------------
-# Author:    	  Michael DeGuzis
-# Git:	    	  https://github.com/ProfessorKaos64/SteamOS-Tools
-# Scipt Name:	  build-pcsx2.sh
-# Script Ver:	  0.1.1
-# Description:	  Attempts to build a deb package from PCSX2 git source
+#-------------------------------------------------------------------------------
+# Author:	Michael DeGuzis
+# Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	build-pcsx2-snapshot.sh
+# Script Ver:	1.0.0
+# Description:	Attempts to builad a deb package from latest pcsx2
+#		bsnes github release
 #
-# See:		  https://code.google.com/p/pcsx2/wiki/CompilationGuideForLinux
-# Usage:	  ./build-pcsx2.sh
-# -------------------------------------------------------------------------------
+# See:		https://github.com/PCSX2/pcsx2
+#
+# Usage:	build-pcsx2-snapshot.sh
+#
+#-------------------------------------------------------------------------------
 
 arg1="$1"
 scriptdir=$(pwd)
 time_start=$(date +%s)
 time_stamp_start=(`date +"%T"`)
-# reset source command for while loop
-src_cmd=""
 
-# vars for package
-pkgname="pcsx2.snapshot"
-#pkgver="20151003+git"
-pkgrev="1"
-dist_rel="brewmaster"
-
-# build dirs
-build_dir="/home/desktop/build-pcsx2-temp"
-git_dir="$build_dir/pcsx2"
+# upstream vars
 git_url="https://github.com/PCSX2/pcsx2"
+branch="master"
 
 # package vars
+date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
+date_short=$(date +%Y%m%d)
+pkgname="pcsx2.snapshot"
+pkgver="${date_short}+git+bsos"
+pkgrev="1"
+dist_rel="brewmaster"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
+maintainer="ProfessorKaos64"
+
+# set build_dir
+build_dir="$HOME/build-${pkgname}-temp"
+git_dir="${build_dir}/${pkgname}"
 
 install_prereqs()
 {
 	clear
 	echo -e "==> Installing prerequisites for building...\n"
 	sleep 2s
-	# install needed packages
-	sudo apt-get install -y --force-yes git devscripts build-essential checkinstall \
-	debian-keyring debian-archive-keyring cmake g++ g++-multilib \
-	libqt4-dev libqt4-dev libxi-dev libxtst-dev libX11-dev bc libsdl2-dev \
-	gcc gcc-multilib nano
+	# install basic build packages
+	sudo apt-get -y --force-yes install build-essential pkg-config bc \
+	cmake debhelper dpkg-dev libaio-dev libasound2-dev libgl1-mesa-dev  \
+	libgtk2.0-dev liblzma-dev libpng++-dev libpng-dev libsdl1.2-dev libsoundtouch-dev\
+	libwxbase3.0-dev libwxgtk3.0-dev libx11-dev portaudio19-dev zlib1g-dev
 
-	echo -e "\n==> Installing pcsx2 build dependencies...\n"
-	sleep 2s
-
-	sudo apt-get install -y --force-yes libaio-dev libpng++-dev libsoundtouch-dev \
-	libwxbase3.0-dev libwxgtk3.0-dev portaudio19-dev
 
 }
 
 main()
 {
 
-	# create and enter build_dir
-
+	# create build_dir
 	if [[ -d "$build_dir" ]]; then
 
 		sudo rm -rf "$build_dir"
@@ -67,11 +65,21 @@ main()
 
 	fi
 
-	# Enter build dir
-	cd "$build_dir"
+	# enter build dir
+	cd "$build_dir" || exit
+
+	# install prereqs for build
+	install_prereqs
+
+	# Clone upstream source code and branch
+
+	echo -e "\n==> Obtaining upstream source code\n"
+
+	# clone
+	git clone -b "$branch" "$git_url" "$git_dir"
 
 	#################################################
-	# Build PKG
+	# Build package
 	#################################################
 
 	echo -e "\n==> Creating original tarball\n"
@@ -79,122 +87,105 @@ main()
 
 	# create the tarball from latest tarball creation script
 	# use latest revision designated at the top of this script
-	wget "https://github.com/PCSX2/pcsx2/raw/master/debian-packager/create_built_tarball.sh"
-	sh "create_built_tarball.sh"
-	rm "create_built_tarball.sh"
 
-	# unpack tarball
-	tar -xf pcsx2*.tar.xz
+	# create source tarball
+	tar -cvzf "${pkgname}_${pkgver}.orig.tar.gz" "${pkgname}"
 
-	# actively get pkg ver from created tarball
-	pkgver=$(find . -name *.orig.tar.xz | cut -c 18-41)
+	# copy in debian folder
+	cp -r $scriptdir/$pkgname/debian "${git_dir}"
 
 	# enter source dir
-	cd pcsx2*
+	cd "${pkgname}"
 
-	# Add in debian folder
-	cp -r $scriptdir/$pkgname/debian .
+	# Create basic changelog format
+	# This addons build cannot have a revision
+	cat <<-EOF> changelog.in
+	$pkgname ($pkgver) $dist_rel; urgency=low
 
-	# Change version, uploader, insert change log comments
-	sed -i "s|version_placeholder|$pkgver-$pkgrev|g" debian/changelog
-	sed -i "s|uploader|$uploader|g" debian/changelog
-	sed -i "s|dist_rel|$dist_rel|g" debian/changelog
+	  * Packaged deb for SteamOS-Tools
+	  * See: packages.libregeek.org
+	  * Upstream authors and source: $git_url
+
+	 -- $uploader  $date_long
+
+	EOF
+
+	# Perform a little trickery to update existing changelog or create
+	# basic file
+	cat 'changelog.in' | cat - debian/changelog > temp && mv temp debian/changelog
 
 	# open debian/changelog and update
-	echo -e "\n==> Opening changelog for build. Please ensure there is a revision number"
+	echo -e "\n==> Opening changelog for confirmation/changes."
 	sleep 3s
 	nano debian/changelog
 
-	############################
-	# proceed to DEB BUILD
-	############################
+ 	# cleanup old files
+ 	rm -f changelog.in
+ 	rm -f debian/changelog.in
 
-	echo -e "\n==> Building Debian package from source\n"
+	#################################################
+	# Build Debian package
+	#################################################
+
+	echo -e "\n==> Building Debian package ${pkgname} from source\n"
 	sleep 2s
 
-	# Build with dpkg-buildpackage
-
-	#dpkg-buildpackage -us -uc -nc
+	#  build
 	dpkg-buildpackage -rfakeroot -us -uc
 
 	#################################################
 	# Post install configuration
 	#################################################
-
-	# TODO
-
+	
 	#################################################
 	# Cleanup
 	#################################################
-
+	
 	# clean up dirs
-
+	
 	# note time ended
 	time_end=$(date +%s)
 	time_stamp_end=(`date +"%T"`)
 	runtime=$(echo "scale=2; ($time_end-$time_start) / 60 " | bc)
-
+	
 	# output finish
 	echo -e "\nTime started: ${time_stamp_start}"
 	echo -e "Time started: ${time_stamp_end}"
 	echo -e "Total Runtime (minutes): $runtime\n"
 
-
+	
 	# assign value to build folder for exit warning below
 	build_folder=$(ls -l | grep "^d" | cut -d ' ' -f12)
-
+	
 	# back out of build temp to script dir if called from git clone
 	if [[ "$scriptdir" != "" ]]; then
-		cd "$scriptdir"
+		cd "$scriptdir" || exit
 	else
-		cd "$HOME"
+		cd "$HOME" || exit
 	fi
-
+	
 	# inform user of packages
 	echo -e "\n############################################################"
 	echo -e "If package was built without errors you will see it below."
 	echo -e "If you don't, please check build dependcy errors listed above."
 	echo -e "############################################################\n"
 	
-	echo -e "Showing contents of: $build_dir: \n"
-	ls "$build_dir"
-
-	echo -e "\n==> Would you like to trim tar.gz, dsc files, and folders for uploading? [y/n]"
-	sleep 0.5s
-	# capture command
-	read -ep "Choice: " trim_choice
-	
-	if [[ "$trim_choice" == "y" ]]; then
-		
-		# cut files so we just have our deb pkg
-		sudo rm -f $git_dir/*.tar.gz
-		sudo rm -f $git_dir/*.dsc
-		sudo rm -f $git_dir/*.changes
-		sudo rm -f $git_dir/*-dbg
-		sudo rm -f $git_dir/*-dev
-		sudo rm -f $git_dirs/*-compat
-
-		# remove source directory that was made
-		find $build_dir -mindepth 1 -maxdepth 1 -type d -exec rm -r {} \;
-
-	elif [[ "$trim_choice" == "n" ]]; then
-
-		echo -e "File trim not requested"
-	fi
+	echo -e "Showing contents of: ${build_dir}: \n"
+	ls ${build_dir}| grep -E *.deb
 
 	echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
 	sleep 0.5s
 	# capture command
-	read -ep "Choice: " transfer_choice
-
+	read -erp "Choice: " transfer_choice
+	
 	if [[ "$transfer_choice" == "y" ]]; then
-
+	
 		# cut files
-		if [[ -d "$build_dir" ]]; then
-			scp $build_dir/*.deb mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
+		if [[ -d "${build_dir}" ]]; then
+			scp ${build_dir}/*.deb mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
 
 		fi
-
+		
 	elif [[ "$transfer_choice" == "n" ]]; then
 		echo -e "Upload not requested\n"
 	fi
@@ -202,5 +193,4 @@ main()
 }
 
 # start main
-install_prereqs
 main

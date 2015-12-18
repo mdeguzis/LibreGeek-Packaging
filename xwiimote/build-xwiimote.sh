89da16fa3ed7a6a649009526f7ab24438f76cd24
mikeyd <mdeguzis@gmail.com>
2015-12-18 16:34:35 -0500

    correct path

--- xwiimote/build-xwiimote.sh
+++ xwiimote/build-xwiimote.sh
@@ -86,7 +86,7 @@ main()
 	tar -cvzf "${pkgname}_${pkgver}.orig.tar.gz" "${pkgname}"
 	
 	# copy in debian folder
-	cp -r $scriptdir/xwiimote/debian "${git_dir}"
+	cp -r $scriptdir/debian "${git_dir}"
 	
 	# enter source dir
 	cd "${pkgname}"

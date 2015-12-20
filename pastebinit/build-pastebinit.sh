cbddebf070781631824c4ce3c0f82283f0e7c40d
ProfessorKaos64 <mdeguzis@gmail.com>
2015-12-20 15:01:52 -0500

    prefer scp

--- pastebinit/build-pastebinit.sh
+++ pastebinit/build-pastebinit.sh
@@ -179,8 +179,7 @@ main()
 
 		# cut files
 		if [[ -d "${build_dir}" ]]; then
-			scp ${build_dir}/${pkgname}_${pkgver}\* mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
-
+			scp ${build_dir}/${pkgname}_${pkgver}* mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
 		fi
 
 	elif [[ "$transfer_choice" == "n" ]]; then

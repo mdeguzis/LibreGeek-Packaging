0e7897fe4a0e6386283b16e7deda09470543141d
mikeyd <mdeguzis@gmail.com>
2015-12-20 18:09:16 -0500

    Update build-lutris.sh

--- lutris/build-lutris.sh
+++ lutris/build-lutris.sh
@@ -26,8 +26,9 @@ rel_target="v0.3.7"
 date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
 date_short=$(date +%Y%m%d)
 pkgname="lutris"
-pkgver="0.3.7+git+bsos"
-pkgrev="1"
+pkgver="0.3.7"
+pkgrev="2"
+pkgsuffix="git+bsos${pkgrev}"
 dist_rel="brewmaster"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
 maintainer="ProfessorKaos64"
@@ -99,7 +100,7 @@ main()
 	# Create basic changelog format
 	# This addons build cannot have a revision
 	cat <<-EOF> changelog.in
-	$pkgname ($pkgver) $dist_rel; urgency=low
+	$pkgname (${pkgver}+${pkgsuffix}) $dist_rel; urgency=low
 
 	  * Packaged deb for SteamOS-Tools
 	  * See: packages.libregeek.org

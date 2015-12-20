2365f92552d53faaaf92da5a406944b39265ab16
mikeyd <mdeguzis@gmail.com>
2015-12-20 15:50:15 -0500

    Update build-pastebinit.sh

--- pastebinit/build-pastebinit.sh
+++ pastebinit/build-pastebinit.sh
@@ -96,7 +96,7 @@ main()
 	# Create basic changelog format
 	# This addons build cannot have a revision
 	cat <<-EOF> changelog.in
-	$pkgname ($pkgver-$pkgrev) $dist_rel; urgency=low
+	$pkgname ($pkgver) $dist_rel; urgency=low
 
 	  * Packaged deb for SteamOS-Tools
 	  * See: packages.libregeek.org

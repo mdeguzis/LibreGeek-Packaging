b6e514239fcf52c1fb0f9ca602f2a66c52c918e1
mikeyd <mdeguzis@gmail.com>
2015-12-22 18:48:04 -0500

    Update build-qtsixa.sh

--- qtsixa/build-qtsixa.sh
+++ qtsixa/build-qtsixa.sh
@@ -174,7 +174,7 @@ main()
 
 		# cut files
 		if [[ -d "${build_dir}" ]]; then
-			scp ${build_dir}/${pkgname}_${pkgver}* mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
+			scp ${build_dir}/*${pkgver}* mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
 		fi
 
 	elif [[ "$transfer_choice" == "n" ]]; then

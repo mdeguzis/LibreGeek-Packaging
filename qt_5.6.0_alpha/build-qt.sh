dfe4d2c489bf6fc0a501c554237415814d622e03
mikeyd <mdeguzis@gmail.com>
2015-12-23 18:38:48 -0500

    Update build-qt.sh

--- qt_5.6.0_alpha/build-qt.sh
+++ qt_5.6.0_alpha/build-qt.sh
@@ -25,6 +25,7 @@ time_stamp_start=(`date +"%T"`)
 
 # upstream vars
 git_url="https://github.com/ProfessorKaos64/qt"
+rel_target="master"
 
 # package vars
 date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")

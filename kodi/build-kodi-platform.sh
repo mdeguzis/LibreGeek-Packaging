5bf1812e8921baa43d7868ec68286ed87445b7d3
mikeyd <mdeguzis@gmail.com>
2015-12-24 12:55:13 -0500

    modify to include full source to pool

--- kodi/build-kodi-platform.sh
+++ kodi/build-kodi-platform.sh
@@ -23,8 +23,9 @@ date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
 date_short=$(date +%Y%m%d)
 pkgname="kodiplatform"
 #pkgver="${date_short}+git"
-pkgver="16.0.0+git"
+pkgver="16.0.0"
 pkgrev="1"
+pkgsuffix="git+bsos${pkgrev}"
 dist_rel="brewmaster"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
 maintainer="ProfessorKaos64"
@@ -91,7 +92,7 @@ main()
 	# Create basic changelog
 	
 	cat <<-EOF> changelog.in
-	$pkgname ($pkgver) $dist_rel; urgency=low
+	$pkgname (${pkgver}+${pkgsuffix}) $dist_rel; urgency=low
 
 	  * Packaged deb for SteamOS-Tools
 	  * See: packages.libregeek.org
@@ -166,23 +167,22 @@ main()
 		echo -e "If package was built without errors you will see it below."
 		echo -e "If you don't, please check build dependcy errors listed above."
 		echo -e "############################################################\n"
-		
-		echo -e "Showing contents of: ${git_dir}/build: \n"
-		ls "${build_dir}" | grep -E *.deb
 	
+		echo -e "Showing contents of: ${build_dir}: \n"
+		ls "${build_dir}" | grep -E *${pkgver}*
+
 		echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
 		sleep 0.5s
 		# capture command
-		read -ep "Choice: " transfer_choice
-		
+		read -erp "Choice: " transfer_choice
+
 		if [[ "$transfer_choice" == "y" ]]; then
-		
+
 			# cut files
 			if [[ -d "${build_dir}" ]]; then
-				scp ${build_dir}/*.deb mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
-	
+				scp ${build_dir}/*${pkgver}* mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
 			fi
-			
+
 		elif [[ "$transfer_choice" == "n" ]]; then
 			echo -e "Upload not requested\n"
 		fi

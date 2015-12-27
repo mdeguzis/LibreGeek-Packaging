606e88585a82f1fa003555b572395e407bfb7159
mikeyd <mdeguzis@gmail.com>
2015-12-26 20:46:59 -0500

    tarball needs full suffix

--- kodi/build-shairplay.sh
+++ kodi/build-shairplay.sh
@@ -84,7 +84,7 @@ main()
 	# use latest revision designated at the top of this script
 
 	# create source tarball
-	tar -cvzf "${pkgname}_${pkgver}.orig.tar.gz" "${pkgname}"
+	tar -cvzf "${pkgname}_${pkgver}+${pkgsuffix}.orig.tar.gz" "${pkgname}"
 
 	# Add debian build folder
 	cp -r "$scriptdir/$pkgname/debian" "${pkgname}"

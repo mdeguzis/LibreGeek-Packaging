d45103ae5b0e55cdeac40b73fe030c6d138154cb
mikeyd <mdeguzis@gmail.com>
2015-12-26 20:51:28 -0500

    udpated with upstream debiann folder, remove rev

--- kodi/build-shairplay.sh
+++ kodi/build-shairplay.sh
@@ -96,7 +96,7 @@ main()
 	# Create basic changelog (no upstream change log from the PPA source)
 	# This addons build cannot have a revision
 	cat <<-EOF> changelog.in
-	$pkgname (${pkgver}+${pkgsuffix}-${upstream_rev}) $dist_rel; urgency=low
+	$pkgname (${pkgver}+${pkgsuffix}) $dist_rel; urgency=low
 
 	  * Packaged deb for SteamOS-Tools
 	  * See: packages.libregeek.org

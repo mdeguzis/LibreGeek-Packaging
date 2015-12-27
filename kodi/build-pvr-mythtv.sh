e13faf78fc37acb7706518d5354a18a5ae833063
mikeyd <mdeguzis@gmail.com>
2015-12-26 20:16:56 -0500

    spelling error

--- kodi/build-pvr-mythtv.sh
+++ kodi/build-pvr-mythtv.sh
@@ -96,7 +96,7 @@ main()
 	
 	# Create basic changelog
 	cat <<-EOF> changelog.in
-	$pkgname (${pkgver}+${pkgsuffix}-${upsteam_rev}) $dist_rel; urgency=low
+	$pkgname (${pkgver}+${pkgsuffix}-${upstream_rev}) $dist_rel; urgency=low
 
 	  * Packaged deb for SteamOS-Tools
 	  * See: packages.libregeek.org

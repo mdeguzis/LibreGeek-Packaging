dc7ff0c8977b0772a9dc1392ac68f9417906e5ee
mikeyd <mdeguzis@gmail.com>
2015-12-27 20:35:40 -0500

    modify to include full source to pool

--- retroarch/retroarch/build-retroarch.sh
+++ retroarch/retroarch/build-retroarch.sh
@@ -21,14 +21,14 @@ time_stamp_start=(`date +"%T"`)
 # upstream vars
 git_url="https://github.com/libretro/RetroArch"
 rel_target="v1.2.2"
-#rel_target="master"
 
 # package vars
 date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
 date_short=$(date +%Y%m%d)
 pkgname="retroarch"
-pkgver="1.2.2+git+bsos"
+pkgver="1.2.2"
 pkgrev="1"
+pkgsuffix="git+bsos${pkgrev}"
 dist_rel="brewmaster"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
 maintainer="ProfessorKaos64"
@@ -116,7 +116,7 @@ main()
 	# Create basic changelog format
 	# This addons build cannot have a revision
 	cat <<-EOF> changelog.in
-	$pkgname ($pkgver) $dist_rel; urgency=low
+	$pkgname (${pkgver}+${pkgsuffix}) $dist_rel; urgency=low
 
 	  * Packaged deb for SteamOS-Tools
 	  * See: packages.libregeek.org
@@ -147,10 +147,6 @@ main()
 	sleep 2s
 
 	dpkg-buildpackage -rfakeroot -us -uc
-
-	#################################################
-	# Post install configuration
-	#################################################
 	
 	#################################################
 	# Cleanup
@@ -186,21 +182,20 @@ main()
 	echo -e "############################################################\n"
 	
 	echo -e "Showing contents of: ${build_dir}: \n"
-	ls ${build_dir}| grep -E *.deb
+	ls "${build_dir}" | grep -E *${pkgver}*
 
 	echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
 	sleep 0.5s
 	# capture command
 	read -erp "Choice: " transfer_choice
-	
+
 	if [[ "$transfer_choice" == "y" ]]; then
-	
+
 		# cut files
 		if [[ -d "${build_dir}" ]]; then
-			scp ${build_dir}/*.deb mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
-
+			scp ${build_dir}/*${pkgver}* mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
 		fi
-		
+
 	elif [[ "$transfer_choice" == "n" ]]; then
 		echo -e "Upload not requested\n"
 	fi

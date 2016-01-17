c94d4a83dae5367694178b552abcec35c4a4dd81
ProfessorKaos64 <mdeguzis@gmail.com>
2016-01-17 15:20:26 -0500

    update ice build to upstream

--- ice/build-ice.sh
+++ ice/build-ice.sh
@@ -3,12 +3,12 @@
 # Author:	Michael DeGuzis
 # Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	build-ice.sh
-# Script Ver:	1.0.0
+# Script Ver:	1.0.9
 # Description:	Builds simple pacakge for using ice
 #
-# See:		http://github.com/ProfessorKaos64/Ice
+# See:		https://github.com/scottrice/Ice
 #
-# Usage:	build-ice.sh
+# Usage:	./build-ice.sh
 #
 #-------------------------------------------------------------------------------
 
@@ -18,16 +18,17 @@ time_start=$(date +%s)
 time_stamp_start=(`date +"%T"`)
 
 # upstream vars
-git_url="https://github.com/ProfessorKaos64/Ice"
-rel_target="brewmaster"
+git_url="https://github.com/scottrice/Ice"
+rel_target="master"
 
 # package vars
 date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
 date_short=$(date +%Y%m%d)
 pkgname="ice"
 pkgver="0.1.0"
+upstream_rev="2"
 pkgrev="1"
-pkgsuffix="git+bsos${pkgrev}"
+pkgsuffix="bsos${pkgrev}"
 dist_rel="brewmaster"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
 maintainer="ProfessorKaos64"
@@ -41,9 +42,20 @@ install_prereqs()
 	clear
 	echo -e "==> Installing prerequisites for building...\n"
 	sleep 2s
+
+	# Avoid libattr garbage for 32 bit package installed by emulators
+	if [[ -f "/usr/share/doc/libattr1/changelog.Debian.gz" ]]; then
+
+		sudo mv "/usr/share/doc/libattr1/changelog.Debian.gz" \
+		"/usr/share/doc/libattr1/changelog.Debian.gz.old" 2> /dev/null
+	fi
+
 	# install basic build packages
+	# Additional suggested packages added per: https://wiki.debian.org/Python/LibraryStyleGuide
 	sudo apt-get install -y --force-yes build-essential bc debhelper \
-	libxrandr2:i386 python-pip python-psutil
+	python-pip python-psutil groff pysteam git python-setuptools dh-python \
+	python-all python-setuptools python-pip python-docutils python-sphinx pysteam
+
 
 }
 
@@ -86,31 +98,44 @@ main()
 	# create source tarball
 	tar -cvzf "${pkgname}_${pkgver}.orig.tar.gz" "${pkgname}"
 
+	# copy in debian folder
+	cp -r $scriptdir/debian "${git_dir}"
+
 	# enter source dir
 	cd "${git_dir}"
 
-	# Create basic changelog format
-	# This addons build cannot have a revision
-	cat <<-EOF> changelog.in
-	$pkgname (${pkgver}-${pkgrev}+${pkgsuffix}) $dist_rel; urgency=low
+	# Create new changelog if we are not doing an autobuild
+	# Also add exceptions for Travis CI build tests
 
-	  * Packaged deb for SteamOS-Tools
-	  * See: packages.libregeek.org
-	  * Simple package for ice curl tool
-	  * Upstream authors and source: $git_url
+	if [[ "$autobuild" != "yes" ]]; then
 
-	 -- $uploader  $date_long
+		cat <<-EOF> changelog.in
+		$pkgname (${pkgver}-${upstream_rev}+${pkgsuffix}) $dist_rel; urgency=low
 
-	EOF
+	  	  * Packaged deb for SteamOS-Tools
+		  * See: packages.libregeek.org
+		  * Upstream authors and source: $git_url
 
-	# Perform a little trickery to update existing changelog or create
-	# basic file
-	cat 'changelog.in' | cat - debian/changelog > temp && mv temp debian/changelog
+		 -- $uploader  $date_long
 
-	# open debian/changelog and update
-	echo -e "\n==> Opening changelog for confirmation/changes."
-	sleep 3s
-	nano debian/changelog
+		EOF
+
+		# Perform a little trickery to update existing changelog or create
+		# basic file
+		cat 'changelog.in' | cat - debian/changelog > temp && mv temp debian/changelog
+
+		# open debian/changelog and update
+		echo -e "\n==> Opening changelog for confirmation/changes."
+		sleep 3s
+		nano debian/changelog
+
+	else
+
+		# Add exceptions for travis ci build. python-appdirs is not yet whitelisted on travis ci
+		# This is installed above manually via .travis.yml
+		sed -ie '/python-appdirs/d' debian/control
+
+	fi
 
  	# cleanup old files
  	rm -f changelog.in
@@ -123,62 +148,66 @@ main()
 	echo -e "\n==> Building Debian package ${pkgname} from source\n"
 	sleep 2s
 
-	dpkg-buildpackage -rfakeroot -us -uc
+	dpkg-buildpackage -rfakeroot -us -uc -sa
 
-	#################################################
-	# Post install configuration
-	#################################################
-	
 	#################################################
 	# Cleanup
 	#################################################
-	
+
 	# clean up dirs
-	
+
 	# note time ended
 	time_end=$(date +%s)
 	time_stamp_end=(`date +"%T"`)
 	runtime=$(echo "scale=2; ($time_end-$time_start) / 60 " | bc)
-	
+
 	# output finish
 	echo -e "\nTime started: ${time_stamp_start}"
 	echo -e "Time started: ${time_stamp_end}"
 	echo -e "Total Runtime (minutes): $runtime\n"
 
-	
+
 	# assign value to build folder for exit warning below
 	build_folder=$(ls -l | grep "^d" | cut -d ' ' -f12)
-	
+
 	# back out of build temp to script dir if called from git clone
 	if [[ "$scriptdir" != "" ]]; then
 		cd "$scriptdir" || exit
 	else
 		cd "$HOME" || exit
 	fi
-	
+
 	# inform user of packages
 	echo -e "\n############################################################"
 	echo -e "If package was built without errors you will see it below."
 	echo -e "If you don't, please check build dependcy errors listed above."
 	echo -e "############################################################\n"
-	
+
 	echo -e "Showing contents of: ${build_dir}: \n"
-	ls ${build_dir}| grep $pkgname_$pkgver
+	ls ${build_dir}| grep ${pkgver}
 
-	echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
-	sleep 0.5s
-	# capture command
-	read -erp "Choice: " transfer_choice
+	if [[ "$autobuild" != "yes" ]]; then
 
-	if [[ "$transfer_choice" == "y" ]]; then
+		echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
+		sleep 0.5s
+		# capture command
+		read -erp "Choice: " transfer_choice
 
-		# cut files
-		if [[ -d "${build_dir}" ]]; then
-			scp ${build_dir}/${pkgname}_${pkgver}* mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
+		if [[ "$transfer_choice" == "y" ]]; then
+
+			# cut files
+			if [[ -d "${build_dir}" ]]; then
+				scp ${build_dir}/*${pkgver}* mikeyd@archboxmtd:/home/mikeyd/packaging/SteamOS-Tools/incoming
+
+				# Preserve changelog
+				cd "$scriptdir"
+				cp "${git_dir}/debian/changelog" ../debian/ 
+
+			fi
+
+		elif [[ "$transfer_choice" == "n" ]]; then
+			echo -e "Upload not requested\n"
 		fi
-
-	elif [[ "$transfer_choice" == "n" ]]; then
-		echo -e "Upload not requested\n"
 	fi
 
 }

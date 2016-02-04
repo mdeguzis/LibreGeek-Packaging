41faeeedefbc64ca6f921523a18019d5aa2f7692
mikeyd <mdeguzis@gmail.com>
2016-02-04 14:18:01 -0500

    use sdl2 pkgs not 1.2

--- openlierox/build-openlierox.sh
+++ openlierox/build-openlierox.sh
@@ -43,7 +43,7 @@ install_prereqs()
 	sleep 2s
 	# install basic build packages
 	sudo apt-get -y --force-yes install autoconf automake build-essential pkg-config bc checkinstall \
-	debhelper bash gzip g++ libsdl1.2-dev libsdl-mixer1.2-dev  libgd2-xpm-dev \
+	debhelper bash gzip g++ libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev libgd2-xpm-dev \
 	libgd2-noxpm-dev zlib1g-dev libxml2-dev libcurl4-openssl-dev libboost-dev libboost-signals-dev \
 	libalut-dev libopenal-dev cmake libhawknl-dev liblua5.1-0-dev libzip-dev binutils-dev libsdl2-image-dev
 

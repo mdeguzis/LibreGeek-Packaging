9a3fd0d1fe50e8b0e7354f2021df0da73d52bb8c
mikeyd <mdeguzis@gmail.com>
2016-02-05 18:35:53 -0500

    use Isengard until Jarvis is out

--- kodi/build-pvr-njoy.sh
+++ kodi/build-pvr-njoy.sh
@@ -18,7 +18,7 @@ time_stamp_start=(`date +"%T"`)
 
 # upstream vars
 git_url="https://github.com/kodi-pvr/pvr.njoy"
-git_branch="master"
+git_branch="Isengard"
 
 # package vars
 date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")

ccae97998162d590b53c25d14a84e115c6593785
mikeyd <mdeguzis@gmail.com>
2016-02-08 14:01:40 -0500

    Update build-pcsx2.sh

--- pcsx2/build-pcsx2.sh
+++ pcsx2/build-pcsx2.sh
@@ -253,7 +253,7 @@ main()
 
 			# Only move the old changelog if transfer occurs to keep final changelog 
 			# out of the picture until a confirmed build is made. Remove if upstream has their own.
-			cp "${git_dir}/debian/changelog" "${scriptdir}/debian-unstable"
+			cp "${git_dir}/debian/changelog" "${scriptdir}/debian"
 
 		fi
 

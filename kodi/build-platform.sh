75adf4891b71a30485771649679142ef43e102eb
mikeyd <mdeguzis@gmail.com>
2016-02-10 09:09:21 -0500

    Update build-platform.sh

--- kodi/build-platform.sh
+++ kodi/build-platform.sh
@@ -18,15 +18,14 @@ time_stamp_start=(`date +"%T"`)
 
 # upstream URL
 git_url="https://github.com/Pulse-Eight/platform/"
-branch_tag="platform-2.0.1"
+branch_tag="p8-platform-2.0.1"
 
 # package vars
 date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
 date_short=$(date +%Y%m%d)
-#pkgname="p8-platform"
-pkgname="platform"
-pkgver="1.0.10"
-pkgrev="4"
+pkgname="p8-platform"
+pkgver="2.0.1"
+pkgrev="1"
 pkgsuffix="git+bsos${pkgrev}"
 dist_rel="brewmaster"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
@@ -71,13 +70,6 @@ main()
 	# Clone upstream source code
 	
 	echo -e "\n==> Obtaining upstream source code\n"
-	
-	# Use rel_target set above for issue 22, in regards to failure to find squish.h when build Kodi
-	# See: https://github.com/Pulse-Eight/platform/pull/22#issuecomment-172975015
-	# Also see:  https://github.com/xbmc/kodi-platform/pull/16
-	# For now, the target package for Jarvis will be platform, not the renamed p8-platform
-
-	# Do NOT remove this rel_target defined above, until Kodi team moves to p8-platform.
 
 	git clone -b "$branch_tag" "$git_url" "$git_dir"
  

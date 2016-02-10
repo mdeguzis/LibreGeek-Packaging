a7b005e0502677175770238670deccb6dfcdfd9e
mikeyd <mdeguzis@gmail.com>
2016-02-10 09:17:35 -0500

    set trap for testing flag

--- kodi/build-kodi-platform.sh
+++ kodi/build-kodi-platform.sh
@@ -7,7 +7,8 @@
 # Description:	Attempts to build a deb package from kodi-platform git source
 #
 # See:		http://www.cyberciti.biz/faq/linux-unix-formatting-dates-for-display/
-# Usage:	build-kodi-platform.sh
+# Usage:	build-kodi-platform.sh [opts]
+# Opts:		[--testing]
 # -------------------------------------------------------------------------------
 
 arg1="$1"
@@ -62,6 +63,14 @@ main()
 
 		mkdir -p "$build_dir"
 
+	fi
+	
+	# Set distribution to testing if specified
+	if [[ "$arg1" == "testing" ]]; then
+	
+		new_dist="${dist_rel}_testing"
+		dist_rel="${new_dist}"
+		
 	fi
 
 	# enter build dir

e81f456ce001bea6e7668a7e53033785c60631cd
mikeyd <mdeguzis@gmail.com>
2016-02-10 15:57:04 -0500

    use files instead of hacky cat/EOF here-doc

--- setup-files/configure-packaging-env.sh
+++ setup-files/configure-packaging-env.sh
@@ -3,13 +3,15 @@
 # Author:	Michael DeGuzis
 # Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	configure-packaging-env.sh
-# Script Ver:	1.3.1
+# Script Ver:	2.1.1
 # Description:	Installs basic packaging tools suggested by the Debian
 #               maintainers guide and configures various packaging options
 #
 # See:		https://www.debian.org/doc/manuals/maint-guide/start.en.html#needprogs
 # Usage:	./configure-packaging-env.sh
 # -------------------------------------------------------------------------------
+
+scriptdir=$(pwd)
 
 clear
 echo -e "==> Installing basic packages"
@@ -24,102 +26,33 @@ autotools-dev debhelper dh-make devscripts fakeroot git lintian patch patchutils
 pbuilder perl python quilt xutils-dev dh-make devscripts
 
 ####################################################################
-# General packaging defaults
+# Dot files
 ####################################################################
 
-echo -e "\n==> Configuring general package defaults\n"
+echo -e "\n==> Configuring dotfiles"
 sleep 2s
 
-# Ask user for repos / vars
-read -erp "Email: " email
-read -erp "First Name: " first_name
-read -erp "Last Name: " first_name
+# .bashrc (if exists)
+if ! grep "##### DEBIAN PACKAGING SETUP #####"; then
 
-# set tmp var for last run, if exists
-repo_src_tmp="$repo_src"
+        cat "$scriptdir/.bashrc" >> "$HOME/.bashrc"
+        
+        echo -e "Seting info for .bashrc"
+        sleep 2s
+        
+        read -erp "Email: " EMAIL
+        read -erp "Full Name: " NAME
+        sed -e "s|EMAIL|$EMAIL|" "$HOME/.bashrc"
+        sed -e "s|NAME|$NAME|" "$HOME/.bashrc"
 
-cat << EOF > $HOME/.bashrc
-##### DEBIAN PACKAGING SETUP #####
-
-# Debian identification
-DEBEMAIL="${email}"
-DEBFULLNAME="$first_name $last_name"
-export DEBEMAIL DEBFULLNAME
-
-# Quilt
-alias dquilt="quilt --quiltrc=${HOME}/.quiltrc-dpkg"
-complete -F _quilt_completion $_quilt_complete_opt dquilt
-EOF
-
-####################################################################
-# Debuild
-####################################################################
-
-echo -e "\n==> Configuring debuild\n"
-sleep 2s
-
-# debuild default options
-cat<<- EOF> $HOME/.devscripts
-##################################################
-# Notes
-##################################################
-# -us   Do not sign the source package.
-# -uc   Do not sign the .changes file.
-# -i[regex]
-# -I[pattern]
-
-# Re: debuild -i, to avoid editor backup files and version control metadata...
-# Source packages using the latest “3.0 (quilt)” and “3.0 (native)” 
-# formats do not have this problem anymore, the -i -I options 
-# are enabled by default for them.
-# For the old (“1.0”) source format, it’s still needed.
-
-# See: 'man debuild'
-
-##################################################
-# Set defaults
-##################################################
-DEBUILD_DPKG_BUILDPACKAGE_OPTS="-us -uc"
-DEBUILD_LINTIAN_OPTS="-i -I --show-overrides"
-EOF
-
-####################################################################
-# Quilt options
-####################################################################
-
-echo -e "\n==> Configuring quilt\n"
-sleep 2s
-
-# Setup Quilt rc file
-
-cat <<-EOF > $HOME/.quiltrc
-for where in ./ ../ ../../ ../../../ ../../../../ ../../../../../; do
-if [ -e ${where}debian/rules -a -d ${where}debian/patches ]; then
-        export QUILT_PATCHES=debian/patches
-        break
 fi
-done
-
-QUILT_PUSH_ARGS="--color=auto"
-QUILT_DIFF_ARGS="--no-timestamps --no-index -p ab --color=auto"
-QUILT_REFRESH_ARGS="--no-timestamps --no-index -p ab"
-QUILT_DIFF_OPTS='-p'
-EOF
 
 # Setup Quilt rc file for dpkg
+cp "$scriptdir/.quiltrc-dpkg" "$HOME"
+cp "$scriptdir/.quiltrc" "$HOME"
 
-cat <<-EOF > $HOME/.quiltrc-dpkg
-d=. ; while [ ! -d $d/debian -a `readlink -e $d` != / ]; do d=$d/..; done
-if [ -d $d/debian ] && [ -z $QUILT_PATCHES ]; then
-    # if in Debian packaging tree with unset $QUILT_PATCHES
-    QUILT_PATCHES="debian/patches"
-    QUILT_PATCH_OPTS="--reject-format=unified"
-    QUILT_DIFF_ARGS="-p ab --no-timestamps --no-index --color=auto"
-    QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"
-    QUILT_COLORS="diff_hdr=1;32:diff_add=1;34:diff_rem=1;31:diff_hunk=1;33:diff_ctx=35:diff_cctx=33"
-    if ! [ -d $d/debian/patches ]; then mkdir $d/debian/patches; fi
-fi
-EOF
+# devscripts
+cp "$scriptdir/.quiltrc" "$HOME"
 
 ####################################################################
 # Pbuilder

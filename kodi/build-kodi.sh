a5781f874e6e9f3822870344a5eef10f2be9a381
mikeyd <mdeguzis@gmail.com>
2016-02-16 13:18:41 -0500

    Update build-kodi.sh

--- kodi/build-kodi.sh
+++ kodi/build-kodi.sh
@@ -347,11 +347,16 @@ kodi_package_deb()
 
 	elif [[ "$build_choice" == "pbuilder" ]]; then
 
+		# Remove options set in upstream's script so we can provide our overrides below
+		sed -i "s|ARCHS=${ARCHS:-$(dpkg --print-architecture)}|d" "tools/Linux/packaging/mk-debian-package.sh"
+		
+		exit 1
+
 		RELEASEV="$kodi_tag" \
 		DISTS="brewmaster" \
 		ARCHS="amd64" \
 		BUILDER="pdebuild" \
-		PDEBUILD_OPTS="--debbuildopts \"-j4\"" \ 
+		PDEBUILD_OPTS="--debbuildopts \"-j4\"" \
 		PBUILDER_BASE="$HOME/pbuilder" \
 		tools/Linux/packaging/mk-debian-package.sh
 

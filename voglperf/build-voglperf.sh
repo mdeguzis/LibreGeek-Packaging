2282d7ef78c2dd7676c66b81ea801edd055030a4
mikeyd <mdeguzis@gmail.com>
2016-02-24 06:31:44 -0500

    start trying out dch for changelogs

--- voglperf/build-voglperf.sh
+++ voglperf/build-voglperf.sh
@@ -119,35 +119,19 @@ main()
 	tar -cvzf "${pkgname}_${pkgver}.orig.tar.gz" "${pkgname}"
 
 	# enter source dir
-	cd "${pkgname}"
+	cd "${git_dir}"
 
-	commits_full=$(git log --pretty=format:"  * %cd %h %s")
 
-	# Create basic changelog format
-	# This addons build cannot have a revision
-	cat <<-EOF> changelog.in
-	$pkgname ($pkgver) $DIST; urgency=low
-
-	  * Packaged deb for SteamOS-Tools
-	  * See: packages.libregeek.org
-	  * Upstream authors and source: $git_url
-
-	 -- $uploader  $date_long
-
-	EOF
-
-	# Perform a little trickery to update existing changelog or create
-	# basic file
-	cat 'changelog.in' | cat - debian/changelog > temp && mv temp debian/changelog
-
-	# open debian/changelog and update
-	echo -e "\n==> Opening changelog for confirmation/changes."
-	sleep 3s
-	nano "debian/changelog"
-
- 	# cleanup old files
- 	rm -f changelog.in
- 	rm -f debian/changelog.in
+	# Create basic changelog format if it does exist or update
+	if [[ -f "debian/changelog" ]]; then
+	
+		dch -i -M --package $pkgname -D $DIST -u low
+		
+	else
+	
+		dch --create -v $pkgver -M --package $pkgname -D $DIST -u low
+	
+	fi
 
 	#################################################
 	# Build Debian package

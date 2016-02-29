4207686bf219949ebdbce1a69c4d5be3f6a33ab0
mikeyd <mdeguzis@gmail.com>
2016-02-29 11:14:15 -0500

    Update darkmod-postinst.sh

--- darkmod/darkmod-postinst.sh
+++ darkmod/darkmod-postinst.sh
@@ -1,101 +1,127 @@
 #!/bin/bash
 
-UPDATER_URL="http://www.fidcal.com/darkuser/"
-UPDATER_ZIP="tdm_update_linux.zip"
-UPDATER_FILE="tdm_update.linux"
-OPTIONS="--noselfupdate"
+fucnt_set_vars()
+{
 
-# check if we are runnign SteamOS or not.
-# Since / is normally only 10 GB, we will not install a multi-GB folder set there
-OS=$(lsb_release -is)
-DEBIAN=(uname -a | grep Debian)
+	UPDATER_URL="http://www.fidcal.com/darkuser/"
+	UPDATER_ZIP="tdm_update_linux.zip"
+	UPDATER_FILE="tdm_update.linux"
+	OPTIONS="--noselfupdate"
 
-if [[ "${OS}" == "SteamOS" ]]; then
+	# check if we are runnign SteamOS or not.
+	# Since / is normally only 10 GB, we will not install a multi-GB folder set there
+	OS=$(lsb_release -is)
+	DEBIAN=(uname -a | grep Debian)
 
-  # create any dirs needed
-  echo -e "Detected SteamOS"
-  GAME_DIR="/home/steam/darkmod"
+	if [[ "${OS}" == "SteamOS" ]]; then
 
-elif [[ "${DEBIAN}" != "" ]]; then
+	  # create any dirs needed
+	  echo -e "Detected SteamOS"
+	  GAME_DIR="/home/steam/darkmod"
+	
+	elif [[ "${DEBIAN}" != "" ]]; then
 
-  echo -e "Detected Other Debian OS"
-  GAME_DIR="/usr/share/games/darkmod"
+	  echo -e "Detected Other Debian OS"
+	  GAME_DIR="/usr/share/games/darkmod"
 
-else
+	else
 
-   echo -e "Detected incompatible OS. Exiting"
-   exit 1
+	   echo -e "Detected incompatible OS. Exiting"
+	   exit 1
 
-fi
+	fi
 
-# create game directory
-mkdir -p "${GAME_DIR}"
+	# create game directory
+	mkdir -p "${GAME_DIR}"
 
-# clean up any old updater files
+	# clean up any old updater files
 
-rm -f "${GAME_DIR}/${UPDATER_ZIP}"
-rm -f "${GAME_DIR}/${UPDATER_FILE}"
+	rm -f "${GAME_DIR}/${UPDATER_ZIP}"
+	rm -f "${GAME_DIR}/${UPDATER_FILE}"
 
-echo -e "\n==> Now running post install steps"
-sleep 2s
+}
 
-# get updater for latest release
+funct_download_updater()
+{
 
-echo -e "\nDownloading The Dark Mod updater...\n"
-sleep 2s
+	# get updater for latest release
 
-wget -O "${GAME_DIR}/${UPDATER_ZIP}" "${UPDATER_URL}/${UPDATER_ZIP}" -q --show-progress -nc
-unzip -d "${GAME_DIR}" "${GAME_DIR}/${UPDATER_ZIP}"
-chmod +x "${GAME_DIR}/${UPDATER_FILE}"
+	echo -e "\nDownloading The Dark Mod updater...\n"
+	sleep 2s
 
-# run updater
-# total size sould be ~2.61 GB
+	wget -O "${GAME_DIR}/${UPDATER_ZIP}" "${UPDATER_URL}/${UPDATER_ZIP}" -q --show-progress -nc
+	unzip -d "${GAME_DIR}" "${GAME_DIR}/${UPDATER_ZIP}"
+	chmod +x "${GAME_DIR}/${UPDATER_FILE}"
 
-###################################
-		# Start web app addition loop
-		###################################
+}
+
+funct_menu()
+{
+
+	###################################
+	# Start web app addition loop
+	###################################
+	
+	while [[ "$choice" != "e" ]];
+	do	
+
+		cat<<-EOF
+		#############################################################
+		The Dark Mod Updater Wraper for Debian GNU/Linux
+		#############################################################
+		Please choose which webapp you wish to add. Choose Done when finished."
+		(1) Update / Install game files
+		(e) Exit / Done
+
+		EOF
+
+		# the prompt sometimes likes to jump above sleep
+		sleep 0.5s
+
+		read -ep "Choice: " choice
+
+		case "$choice" in
 		
-		while [[ "$web_app_choice" != "9" || "$web_app_choice" != "done" ]];
-		do	
-		
-		  cat<<-EOF
-			#############################################################
-			The Dark Mod Updater Wraper for Debian GNU/Linux
-			#############################################################
-	    Please choose which webapp you wish to add. Choose Done when finished."
-		  (1) Updater / Install game files
-			
-			EOF
-			
-			# the prompt sometimes likes to jump above sleep
-			sleep 0.5s
-			
-			read -ep "Choice: " choice
-			
-			case "$choice" in
-			        
-			        1)
-			        echo -e "\nRunning updater in game dir: ${GAME_DIR}\n"
-              cd ${GAME_DIR} || exit
-              ./${UPDATER_FILE} ${OPTIONS}
-			        kiosk_opt="/usr/bin/$LAUNCHER"
-			        ;;
-			        
+		1)
+		echo -e "\nRunning updater in game dir: ${GAME_DIR}\n"
+		cd ${GAME_DIR} || exit
+		./${UPDATER_FILE} ${OPTIONS}
+		funct_cleanup
+		;;
 
-			         
-			        *)
-			        echo -e "\n==ERROR==\nInvalid Selection!"
-			        sleep 1s
-			        continue
-				;;
-			esac
+		e)
+		echo -e "\nExiting\n"
+		exit 1
+		;;
 
-echo -e "\nRunning cleanup\n"
-sleep 2s
+		*)
+		echo -e "\n==ERROR==\nInvalid Selection!\n"
+		sleep 1s
+		continue
+		;;
+		esac
 
-if [[ "${OS}" == "SteamOS" ]]; then
+	done
 
-  # Fix owner, perms on GAME_DIR
-  chown -R steam:steam ${GAME_DIR}
- 
-fi
+}
+
+funct_cleanup()
+{
+
+	echo -e "\nRunning cleanup\n"
+	sleep 2s
+
+	if [[ "${OS}" == "SteamOS" ]]; then
+	
+	  # Fix owner, perms on GAME_DIR
+	  chown -R steam:steam ${GAME_DIR}
+
+	fi
+	
+}
+
+# Start script
+funct_set_vars
+funct_download_update
+funct_menu
+funct_cleanup

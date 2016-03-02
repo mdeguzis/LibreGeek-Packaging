761cdca2e6df4be886115511067dca0905526b72
SteamOS Desktop <ProfessorKaos64>
2016-03-02 15:59:01 -0500

    add base citra files

---
+++ citra/build-citra.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+# Author:	Michael DeGuzis
+# Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt name:	build-ut4.sh
+# Script Ver:	0.1.1
+# Description:	Attempts to build a deb package from the latest UT4 Linux
+#		release.
+#
+# See:		https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=ut4
+#		https://forums.unrealtournament.com/showthread.php?12011-Unreal-Tournament-Pre-Alpha-Playable-Build		
+#
+# Usage:	./build-ut4.sh
+# Opts:		[--testing]
+#		Modifys build script to denote this is a test package build.
+# -------------------------------------------------------------------------------
+
+#################################################
+# Set variables
+#################################################
+
+arg1="$1"
+scriptdir=$(pwd)
+time_start=$(date +%s)
+time_stamp_start=(`date +"%T"`)
+
+# repo destination vars (use only local hosts!)
+USER="mikeyd"
+HOST="archboxmtd"
+
+if [[ "$arg1" == "--testing" ]]; then
+
+	REPO_FOLDER="/home/mikeyd/packaging/SteamOS-Tools/incoming_testing"
+
+else
+
+	REPO_FOLDER="/home/mikeyd/packaging/SteamOS-Tools/incoming"
+
+fi
+# upstream vars
+#git_url="https://github.com/ProfessorKaos64/tdm"
+#branch="master"
+
+# package vars
+date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
+date_short=$(date +%Y%m%d)
+ARCH="amd64"
+BUILDER="pdebuild"
+BUILDOPTS="--debbuildopts -b"
+pkgname="ut4"
+pkgver="${date_short}"
+pkgrev="1"
+upstream_rev="1"
+# Base version sourced from ZIP file version
+pkgsuffix="2883976build+alpha+bsos${pkgrev}"
+DIST="brewmaster"
+urgency="low"
+uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
+maintainer="ProfessorKaos64"
+
+# Need network for pbuilder to pull down ut4 zip
+export NETWORK="yes"
+
+# set build directories
+export build_dir="${HOME}/build-${pkgname}-temp"
+src_dir="${pkgname}-${pkgver}"
+git_dir="${build_dir}/${src_dir}"
+
+install_prereqs()
+{
+	clear
+	echo -e "==> Installing prerequisites for building...\n"
+	sleep 2s
+	# install basic build packages
+	sudo apt-get install -y --force-yes build-essential pkg-config bc debhelper
+
+}
+
+main()
+{
+
+	# create build_dir
+	if [[ -d "${build_dir}" ]]; then
+
+		sudo rm -rf "${build_dir}"
+		mkdir -p "${build_dir}"
+
+	else
+
+		mkdir -p "${build_dir}"
+
+	fi
+
+	# enter build dir
+	cd "${build_dir}" || exit
+
+	# install prereqs for build
+	if [[ "${BUILDER}" != "pdebuild" ]]; then
+
+		# handle prereqs on host machine
+		install_prereqs
+
+	fi
+
+	# Clone upstream source code and branch
+	# NOTE! - If you wish to source versions or commits automatically into variables here,
+	# 	  such as commits, of upstream tags, see docs/pkg-versioning.md
+
+	echo -e "\n==> Obtaining upstream source code\n"
+
+	mkdir -p "${git_dir}"
+	cp "${scriptdir}/ut4-alpha.png" "${git_dir}"
+	cp "${scriptdir}/ut4-launch.sh" "${git_dir}/ut4-launch"
+
+	#################################################
+	# Build package
+	#################################################
+
+	echo -e "\n==> Creating original tarball\n"
+	sleep 2s
+
+	# create source tarball
+	cd "${build_dir}" || exit
+	tar -cvzf "${pkgname}_${pkgver}+${pkgsuffix}.orig.tar.gz" "${src_dir}"
+
+	# Add debian folder for current virtual package implementation
+	cp -r "${scriptdir}/debian" "${git_dir}"
+
+	# enter source dir
+	cd "${git_dir}"
+
+	echo -e "\n==> Updating changelog"
+	sleep 2s
+
+ 	# update changelog with dch
+	if [[ -f "debian/changelog" ]]; then
+
+		dch -v "${pkgver}+${pkgsuffix}-${upstream_rev}" -M --package "${pkgname}" -D "${DIST}" -u "${urgency}"
+
+	else
+
+		dch --create -v "${pkgver}+${pkgsuffix}-${upstream_rev}" -M --package "${pkgname}" -D "${DIST}" -u "${urgency}"
+
+	fi
+
+	#################################################
+	# Build Debian package
+	#################################################
+
+	echo -e "\n==> Building Debian package ${pkgname} from source\n"
+	sleep 2s
+
+	USENETWORK=$NETWORK DIST=$DIST ARCH=$ARCH ${BUILDER} ${BUILDOPTS}
+
+	#################################################
+	# Cleanup
+	#################################################
+
+	# clean up dirs
+
+	# note time ended
+	time_end=$(date +%s)
+	time_stamp_end=(`date +"%T"`)
+	runtime=$(echo "scale=2; ($time_end-$time_start) / 60 " | bc)
+
+	# output finish
+	echo -e "\nTime started: ${time_stamp_start}"
+	echo -e "Time started: ${time_stamp_end}"
+	echo -e "Total Runtime (minutes): $runtime\n"
+
+
+	# assign value to build folder for exit warning below
+	build_folder=$(ls -l | grep "^d" | cut -d ' ' -f12)
+
+	# inform user of packages
+	cat<<- EOF
+	#################################################################
+	If package was built without errors you will see it below.
+	If you don't, please check build dependency errors listed above.
+	#################################################################
+
+	EOF
+
+	echo -e "Showing contents of: ${build_dir}: \n"
+	ls "${build_dir}" | grep -E *${pkgver}*
+
+	echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
+	sleep 0.5s
+	# capture command
+	read -erp "Choice: " transfer_choice
+
+	if [[ "$transfer_choice" == "y" ]]; then
+
+		if [[ -d "${build_dir}" ]]; then
+
+			# copy files to remote server
+			rsync -arv --filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" ${build_dir}/ ${USER}@${HOST}:${REPO_FOLDER}
+
+			# uplaod local repo changelog
+			cp "${git_dir}/debian/changelog" "${scriptdir}/debian"
+
+		fi
+
+	elif [[ "$transfer_choice" == "n" ]]; then
+		echo -e "Upload not requested\n"
+	fi
+
+}
+
+# start main
+main
---
+++ citra/readme.md
@@ -0,0 +1,146 @@
+# Important
+
+Warning: Please read the below carefully!
+
+Make sure you linked your GitHub account to your profile on
+https://unrealengine.com, as all Epic repositories are private. The script
+may ask you to install additional packages (on certain distributions), then
+it will download the archives with binary dependencies (by default, to
+~/Downloads), which are pretty large (3GB in total) - they won't be re-downloaded
+unless they are updated for the particular release tag. If any new archives have
+been downloaded (which is the case the first time you do this), the script will
+unpack them over your repository (it will ask you to confirm that action as it
+is potentially destructive). You can force this by supplying -updatedeps flag
+if for some reason you want to unpack them again.
+
+# Old code:
+
+	
+	# See:	
+	# https://forums.unrealtournament.com/showthread.php?14240-How-to-run-UT4-Alpha-build
+	# https://forums.unrealtournament.com/showthread.php?12011-Unreal-Tournament-Pre-Alpha-Playable-Build
+	
+	# Archive source can also be generally noted from:
+	# https://aur.archlinux.org/packages/ut4/
+	
+	current_dir=$(pwd)
+	URL="https://s3.amazonaws.com/unrealtournament"
+	ut4_zip="UnrealTournament-Client-XAN-2771652-Linux.zip"
+	ut4_dir="/home/desktop/ut4-linux/LinuxNoEditor"
+	ut4_bin_dir="$ut4_dir/LinuxNoEditor/Engine/Binaries/Linux/"
+	ut4_exe="./UE4-Linux-Test UnrealTournament -SaveToUserDir"
+	
+	ut4_shortcut_tmp="/home/desktop/ue4-alpha.desktop"
+	ut4_cli_tmp="/home/desktop/ue4-alpha"
+	
+	# start apt mode if check
+	if [[ "$apt_mode" == "install" ]]; then
+	
+		if [[ -d "$ut4_dir" ]]; then
+			# DIR exists
+			echo -e "\nUT4 Game directory found"
+		else
+			mkdir -p "$ut4_dir"
+		fi
+		
+		cd "$ut4_dir" || exit
+	
+		#################################################
+		# Prerequisites
+		#################################################
+		
+		#################################################
+		# Gather files
+		#################################################
+		
+		# sourced from https://aur.archlinux.org/packages/ut4/
+		
+		echo -e "\n==> Acquiring files...please wait\n"
+		
+		sleep 2
+		
+		echo -e "${ut4_zip}"
+		wget -O "${ut4_zip}" "${URL}/${ut4_zip}"
+		
+		#################################################
+		# Setup
+		#################################################
+
+		unzip -o "${ue4_zip}"
+		
+		# Mark main binary as executable
+		chmod +x "$ut4_bin_dir/UE4-Linux-Test"
+		
+		#################################################
+		# Post install configuration
+		#################################################
+		
+		echo -e "\n==> Creating executable and desktop launcher"
+		
+		# copy ue4.png into Steam Pictures dir
+		sudo cp "$scriptdir/artwork/games/ut4-alpha.png" "/home/steam/Pictures"
+		
+		cat <<-EOF> ${ue4_cli_tmp}
+		#!/bin/bash
+		# execute ut4 alpha
+		cd $ut4_bin_dir
+		$ut4_exe
+		EOF
+		
+		cat <<-EOF> ${ue4_shortcut_tmp}
+		[Desktop Entry]
+		Name=UT4 alpha
+		Comment=Launcher for UT4 Tournament alpha
+		Exec=/usr/bin/ut4-alpha
+		Icon=/home/steam/Pictures/ut4-alpha.png
+		Terminal=false
+		Type=Application
+		Categories=Game;
+		MimeType=x-scheme-handler/steam;
+		EOF
+		
+		# mark exec
+		chmod +x ${ut4_cli_tmp}
+		
+		# move tmp var files into target locations
+		sudo mv ${ut4_shortcut_tmp}  "/usr/share/applications"
+		sudo mv ${u4_cli_tmp} "/usr/bin"
+		
+		#################################################
+		# Cleanup
+		#################################################
+		
+		# clean up dirs
+		
+		# return to previous dir
+		cd $current_dir || exit
+		
+		clear
+		cat <<-EOF
+		-----------------------------------------------------------------------
+		Summary
+		-----------------------------------------------------------------------
+
+		Installation is finished. You can either run 'ue4-alpha' from
+		the command line, or start 'UE4 aplha' from you applications directory.
+		The launcher should show up as a non-Steam game as well in SteamOS BPM.
+		
+		EOF
+	
+	
+	elif [[ "$apt_mode" == "remove" ]]; then
+	
+		# Add dekstop launcher file here
+		# Transfer to /usr/share/applications
+		# TODO !!!
+		
+		#Remove directories, build files, etc if uninstall is specified by user
+		echo -e "\n==> Removing related files for UE4-src routine"
+		sleep 2s
+		
+		# remove directories
+		sudo rm -rf "/usr/share/applications/ut4-alpha.desktop" "/usr/bin/ut4-alpha" \
+		"$ut4_dir"
+		
+		# end apt mode if check  
+	fi

61bb110c857db32e7f111c39af811c36a17c7a93
mikeyd <mdeguzis@gmail.com>
2016-03-03 13:58:32 -0500

    Update setup-pbuilder.sh

--- setup-files/setup-pbuilder.sh
+++ setup-files/setup-pbuilder.sh
@@ -29,26 +29,51 @@ sleep 2s
 echo -e "==> Installing keyrings\n"
 sleep 2s
 
-# Obtain valve keyring
-wget "http://repo.steamstatic.com/steamos/pool/main/v/valve-archive-keyring/valve-archive-keyring_0.5+bsos3_all.deb" -q --show-progress -nc
-sudo dpkg -i "valve-archive-keyring_0.5+bsos3_all.deb"
+# Set vars
+valve_keyring="valve-archive-keyring_0.5+bsos3_all"
 
-# Libregeek keyrings
-wget http://packages.libregeek.org/libregeek-archive-keyring-latest.deb -q --show-progress -nc
-wget http://packages.libregeek.org/steamos-tools-repo-latest.deb -q --show-progress -nc
-sudo dpkg -i libregeek-archive-keyring-latest.deb
-sudo dpkg -i steamos-tools-repo-latest.deb
+# Test OS first, so we can allow configuration on multiple distros
+OS=$(lsb_release -si)
 
-# cleanup
-rm -f steamos-tools-repo-latest.deb
-rm -f libregeek-archive-keyring-latest.deb
+if [[ "${OS}" == "SteamOS" || "${OS}" == "Debian" ]]; then
 
-# update for keyrings
+	# Obtain valve keyring
+	wget "http://repo.steamstatic.com/steamos/pool/main/v/valve-archive-keyring/${valve_keyring}.deb" -q --show-progress -nc 
+	sudo dpkg -i "valve-archive-keyring_0.5+bsos3_all.deb"
+	
+	# Libregeek keyrings
+	wget http://packages.libregeek.org/libregeek-archive-keyring-latest.deb -q --show-progress -nc
+	wget http://packages.libregeek.org/steamos-tools-repo-latest.deb -q --show-progress -nc
+	sudo dpkg -i libregeek-archive-keyring-latest.deb
+	sudo dpkg -i steamos-tools-repo-latest.deb
+	
+	# cleanup
+	rm -f steamos-tools-repo-latest.deb
+	rm -f libregeek-archive-keyring-latest.deb
+	rm -f valve-archive-keyring*.deb
+	
+	# update for keyrings
+	
+	echo -e "\n==> Updating system for newly added keyrings\n"
+	sleep 2s
+	sudo apt-key update
+	sudo apt-get update
 
-echo -e "\n==> Updating system for newly added keyrings\n"
-sleep 2s
-sudo apt-key update
-sudo apt-get update
+elif [[ "${OS}" == "Arch"]]; then
+
+	
+	# obtain keyring source for Valve archive keyring and convert it, not provided in AUR
+	wget "http://repo.steamstatic.com/steamos/pool/main/v/valve-archive-keyring/${valve_keyring}.deb" -q --show-progress -nc 
+	
+	# Convert
+	ar xv "${valve_keyring}.deb"
+	sudo cp "etc/apt/trusted.gpg.d/valve-archive-keyring.gpg" "/etc/apt/trusted.gpg.d/"
+	sudo cp "usr/share/keyrings/valve-archive-keyring.gpg" "/usr/share/keyrings"
+	
+	# cleanup
+	rm -f valve-archive-keyring*.deb
+
+fi
 
 #####################################
 # PBUILDER setup

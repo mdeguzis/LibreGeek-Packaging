816e1a0d678ffaaa8917bd8e58af788d5e588b4c
mikeyd <mdeguzis@gmail.com>
2016-03-08 11:01:04 -0500

    only switch if using a brewmaster dist

--- setup-files/hooks/D01steamos-tools-hook.sh
+++ setup-files/hooks/D01steamos-tools-hook.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# Add SteamOS tools into chroot environment
+# Add SteamOS tools into chroot environment if we are using a brewmaster DIST
 
-echo "Adding SteamOS-Tools repository configuration"
-sleep 3s
+if [[ "$DIST" == "brewmaster" ]]; then
 
-# get repository configuration script
-wget https://raw.githubusercontent.com/ProfessorKaos64/SteamOS-Tools/brewmaster/configure-repos.sh
-chmod +x configure-repos.sh
-sed -i "s|sudo ||g" configure-repos.sh
-./configure-repos.sh
-
-if [[ "$STEAMOS_TOOLS_BETA_HOOK" == "true" ]]; then
-
-  echo "Switching to SteamOS-Tools beta track"
+  echo "Adding SteamOS-Tools repository configuration"
   sleep 3s
-  apt-get install steamos-tools-beta-repo
-  apt-get update
+  
+  # get repository configuration script
+  wget https://raw.githubusercontent.com/ProfessorKaos64/SteamOS-Tools/brewmaster/configure-repos.sh
+  chmod +x configure-repos.sh
+  sed -i "s|sudo ||g" configure-repos.sh
+  ./configure-repos.sh
+  
+  if [[ "$STEAMOS_TOOLS_BETA_HOOK" == "true" ]]; then
+  
+    echo "Switching to SteamOS-Tools beta track"
+    sleep 3s
+    apt-get install steamos-tools-beta-repo
+    apt-get update
+  
+  fi
   
 fi

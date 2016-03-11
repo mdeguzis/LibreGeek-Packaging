1585d249adafe86b990f83c9df7b98ba574ae389
mikeyd <mdeguzis@gmail.com>
2016-03-11 11:47:18 -0500

    Create temp.sh

---
+++ temp.sh
@@ -0,0 +1,24 @@
+# replacing block of text for user host params, since digital ocean droplet is up and running
+# delete this file when confirmed working
+
+# Remove old vars to make this easy
+find . -name "build*.sh" -print- | xargs -0 sed -i '/USER\=\"mikeyd\"/d'
+find . -name "build*.sh" -print- | xargs -0 sed -i '/HOST\=\"archboxmtd\"/d'
+
+cat<<- EOF> nextext
+
+# Check if USER/HOST is setup under ~/.bashrc, set to default if blank
+# This keeps the IP of the remote VPS out of the build script
+
+if [[ "${USER}" == "" || "${HOST}" == "" ]]; then
+
+	# fallback to local repo pool target(s)
+	USER="mikeyd"
+	HOST="archboxmtd"
+
+fi
+
+EOF
+
+# Add new block of cheese, errrrr....text...
+find . -name "build*.sh" -print- | xargs -0 perl -pe 's|# repo destination|`cat newtext`|e' 

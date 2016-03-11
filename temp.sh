05954542b4f26d480f34d13e28cafd46fe640d15
mikeyd <mdeguzis@gmail.com>
2016-03-11 13:05:51 -0500

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -5,7 +5,7 @@
 find . -name "build*.sh" -print0 | xargs -0 sed -i '/USER\=\"mikeyd\"/d'
 find . -name "build*.sh" -print0 | xargs -0 sed -i '/HOST\=\"archboxmtd\"/d'
 
-echo -e<<-EOF> newtext
+echo -e<<-EOF>newtext
 
 # Check if USER/HOST is setup under ~/.bashrc, set to default if blank
 # This keeps the IP of the remote VPS out of the build script
@@ -21,7 +21,7 @@ fi
 EOF
 
 # Add new block of cheese, errrrr....text...
-find . -name "build*.sh" -print0 | xargs -0 perl -pe 's/.*repo destination.*/`cat newtext`/e' -i
+find . -name "build*.sh" -print0 | xargs -0 perl -pe 's|.*repo destination.*|`cat newtext`|e' -i
 
 # Replace final text in build script
 find . -name "build*.sh" -print0 | xargs -0 sed -i 's/${USER}@${HOST}/${REMOTE_USER}@${REMOTE_HOST}/g'

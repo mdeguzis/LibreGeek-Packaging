368f68017c5af43d0c3544fa4a85437724b11123
ProfessorKaos64 <mdeguzis@gmail.com>
2016-03-16 20:46:28 -0400

    more work on compiling rust

--- rust/build-rust.sh
+++ rust/build-rust.sh
@@ -3,8 +3,9 @@
 # Author:	Michael DeGuzis
 # Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	build-rustc.sh
-# Script Ver:	1.1.9
-# Description:	Attempts to build a deb package from backported stretch package
+# Script Ver:	1.1.5
+# Description:	Attempts to build a deb package from latest rust
+#		github release
 #
 # See:		https://github.com/rust-lang/rust
 #
@@ -17,9 +18,11 @@
 # Set variables
 #################################################
 
-scriptdir="${PWD}"
+arg1="$1"
+scriptdir=$(pwd)
 time_start=$(date +%s)
 time_stamp_start=(`date +"%T"`)
+
 
 # Check if USER/HOST is setup under ~/.bashrc, set to default if blank
 # This keeps the IP of the remote VPS out of the build script
@@ -34,6 +37,7 @@ if [[ "${REMOTE_USER}" == "" || "${REMOTE_HOST}" == "" ]]; then
 fi
 
 
+
 if [[ "$arg1" == "--testing" ]]; then
 
 	REPO_FOLDER="/home/mikeyd/packaging/SteamOS-Tools/incoming_testing"
@@ -44,42 +48,23 @@ else
 
 fi
 
-# Gather vars
-echo -e "\n==> Setting vars\n"
-
-echo -e "\nPress ENTER to use last: ${OLD_PKGNAME}"
-read -erp "Target package name: " PKGNAME
-if  [[ "${PKGNAME}" == "" ]]; then PKGNAME="${OLD_PKGNAME}"; fi
-export OLD_PKGNAME="${PKGNAME}"
-
-echo -e "\nPress ENTER to use last: ${OLD_PKGVER}"
-read -erp "Target package version: " PKGVER
-if [[ "${PKGVER}" == "" ]]; then PKGVER="${OLD_PKGVER}"; fi
-export OLD_PKGVER="${PKGVER}"
-
-echo -e "\nPress ENTER to use last: ${OLD_ARCH}"
-read -erp "Arch target: " ARCH
-if  [[ "${ARCH}" == "" ]]; then ARCH="${OLD_ARCH}"; fi
-export OLD_ARCH="${ARCH}"
-
-echo -e "\nPress ENTER to use last: ${OLD_DSC}"
-read -erp "Paste link to upsteam .dsc: " DSC
-if  [[ "${DSC}" == "" ]]; then DSC="${OLD_DSC}"; fi
-export OLD_DSC="${DSC}"
+# upstream vars
+git_url="https://github.com/rust-lang/rust"
+branch="1.7.0"
 
 # package vars
 date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
 date_short=$(date +%Y%m%d)
-ARCH="$ARCH"
-BUILDER="pbuilder"
+ARCH="amd64"
+BUILDER="pdebuild"
+BUILDOPTS=""
 export STEAMOS_TOOLS_BETA_HOOK="false"
-pkgname="$PKGNAME"
-pkgver="$PKGVER"
+pkgname="rustc"
+pkgver="${branch}"
 upstream_rev="1"
 pkgrev="1"
-pkgsuffix="bpo8+bsos${pkgrev}"
-DIST="jessie-backports"
-DSC_FILENAME=$(basename "${DSC}")
+pkgsuffix="bsos${pkgrev}"
+DIST="brewmaster"
 urgency="low"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
 maintainer="ProfessorKaos64"
@@ -95,7 +80,8 @@ install_prereqs()
 	echo -e "==> Installing prerequisites for building...\n"
 	sleep 2s
 	# install basic build packages
-	sudo apt-get -y --force-yes install autoconf automake build-essential pkg-config bc debhelper
+	sudo apt-get -y --force-yes install autoconf automake build-essential pkg-config bc debhelper \
+ 	g++ clang++ python make curl git
 
 }
 
@@ -119,7 +105,7 @@ main()
 
 	# install prereqs for build
 
-	if [[ "${BUILDER}" != "pdebuild" && "${BUILDER}" != "pbuilder" ]]; then
+	if [[ "${BUILDER}" != "pdebuild" ]]; then
 
 		# handle prereqs on host machine
 		install_prereqs
@@ -130,51 +116,75 @@ main()
 
 	echo -e "\n==> Obtaining upstream source code\n"
 
-	# Obtain all necessary vias via dget
-	dget "${DSC}"
+	# clone
+	git clone -b "${branch}" "${git_url}" "${git_dir}"
+
+	#################################################
+	# Build package
+	#################################################
+
+	echo -e "\n==> Creating original tarball\n"
+	sleep 2s
+
+	# create source tarball
+	cd "${build_dir}"
+	tar -cvzf "${pkgname}_${pkgver}+${pkgsuffix}.orig.tar.gz" "${src_dir}"
+
+	# enter source dir
+	cd "${git_dir}"
+
+	echo -e "\n==> Updating changelog"
+	sleep 2s
+
+	# Create basic changelog format if it does exist or update
+	if [[ -f "debian/changelog" ]]; then
+
+		dch -p --force-distribution -v "${pkgver}+${pkgsuffix}" --package "${pkgname}" -D $DIST -u "${urgency}" \
+		"Initial upload attempt"
+		nano "debian/changelog"
+
+	else
+
+		dch -p --force-distribution --create -v "${pkgver}+${pkgsuffix}" --package "${pkgname}" -D "${DIST}" \
+		-u "${urgency}" "Initial upload attempt"
+		nano "debian/chanelog"
+
+	fi
 
 	#################################################
 	# Build Debian package
 	#################################################
 
-	echo -e "\n==> Backporting Debian package ${pkgname} from source\n"
+	echo -e "\n==> Building Debian package ${pkgname} from source\n"
 	sleep 2s
 
 	#  build
-	wget "${DSC}" -q -nc --show-progress
-
-	if ! sudo build_dir=${build_dir} DIST=${DIST} ARCH=${ARCH} "${BUILDER}" build \
-	"${DSC_FILENAME}" && rm -f ${DSC_FILENAME}; then
-
-		# back out to scriptdir
-		cd "${scritpdir}"
-
-	fi
+	DIST=$DIST ARCH=$ARCH ${BUILDER} ${BUILDOPTS}
 
 	#################################################
 	# Cleanup
 	#################################################
-
+	
 	# note time ended
 	time_end=$(date +%s)
 	time_stamp_end=(`date +"%T"`)
 	runtime=$(echo "scale=2; ($time_end-$time_start) / 60 " | bc)
-
+	
 	# output finish
 	echo -e "\nTime started: ${time_stamp_start}"
 	echo -e "Time started: ${time_stamp_end}"
 	echo -e "Total Runtime (minutes): $runtime\n"
-
+	
 	# inform user of packages
 	cat<<-EOF
-
+	
 	###############################################################
 	If package was built without errors you will see it below.
 	If you don't, please check build dependcy errors listed above.
 	###############################################################
-
+	
 	Showing contents of: ${build_dir}
-
+	
 	EOF
 
 	ls "${build_dir}" | grep -E "${pkgver}" 
@@ -201,6 +211,3 @@ main()
 
 # start main
 main
-
-#In case the script fails cd back to scriptdir
-cd "${scriptdir}"

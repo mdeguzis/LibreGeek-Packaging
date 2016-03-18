d2d95973959c47c8eafba7b0ae53ed0d62eda4a0
mikeyd <mdeguzis@gmail.com>
2016-03-18 09:09:02 -0400

    Update pbulider-build-workaround.sh

--- retroarch/libretro-rustation/pbulider-build-workaround.sh
+++ retroarch/libretro-rustation/pbulider-build-workaround.sh
@@ -4,5 +4,8 @@
 # See: https://github.com/rust-lang/cargo/issues/2492
 
 # Login to the chroot and do build there
-mkdir -p temp_results
-sudo build_dir=temp_results STEAMOS_TOOLS_BETA_HOOK="true" DIST="brewmaster" pbuilder --execute build-libretro-rustation-debuild.sh
+pkgname="libretro-rustation"
+export build_dir="${HOME}/build-${pkgname}-temp"
+mkdir -p $build_dir
+
+sudo -E build_dir=$build_dir STEAMOS_TOOLS_BETA_HOOK="true" DIST="brewmaster" pbuilder --execute build-libretro-rustation-debuild.sh

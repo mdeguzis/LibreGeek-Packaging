3f2cbd164bb7d61d1a51184e78c67be1c733ee7b
mikeyd <mdeguzis@gmail.com>
2016-03-18 09:19:40 -0400

    Update pbulider-build-workaround.sh

--- retroarch/libretro-rustation/pbulider-build-workaround.sh
+++ retroarch/libretro-rustation/pbulider-build-workaround.sh
@@ -8,4 +8,4 @@ pkgname="libretro-rustation"
 export build_dir="${HOME}/build-${pkgname}-temp"
 mkdir -p $build_dir
 
-sudo -E build_dir=$build_dir STEAMOS_TOOLS_BETA_HOOK="true" DIST="brewmaster" pbuilder --execute build-libretro-rustation-debuild.sh
+sudo -E build_dir=$build_dir STEAMOS_TOOLS_BETA_HOOK="true" DIST="brewmaster" pbuilder --execute build-libretro-rustation-pdebuild.sh

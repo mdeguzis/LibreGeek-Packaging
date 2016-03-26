5dc75de81ac4fbf9ad8f98c6017057820ea399ef
mikeyd <mdeguzis@gmail.com>
2016-03-26 12:57:48 -0400

    Update build-plex-media-player.sh

--- plexmediaplayer/build-plex-media-player.sh
+++ plexmediaplayer/build-plex-media-player.sh
@@ -143,7 +143,7 @@ main()
 
 	# create source tarball
 	cd "${build_dir}"
-	tar -cvzf "${pkgname}_${pkgver}.orig.tar.gz" "${src_dir}"
+	tar -cvzf "${pkgname}_${pkgver}+${pkgsuffix}.orig.tar.gz" "${src_dir}"
 
 	# enter source dir
 	cd "${src_dir}"

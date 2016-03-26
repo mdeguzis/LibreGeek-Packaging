af65b2dadadcae1de0da90b0541120bfb0ebabd3
mikeyd <mdeguzis@gmail.com>
2016-03-26 08:38:38 -0400

    Update build-cmake.sh

--- cmake/build-cmake.sh
+++ cmake/build-cmake.sh
@@ -54,13 +54,13 @@ date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
 date_short=$(date +%Y%m%d)
 ARCH="amd64"
 BUILDER="pdebuild"
-BUILDOPTS=""
+BUILDOPTS="--debbuildopts -b"
 export STEAMOS_TOOLS_BETA_HOOK="false"
 pkgname="cmake"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
-pkgver="3.4.1"
-pkgrev="2"
-pkgsuffix="git+bsos${pkgrev}"
+pkgver="3.5.0"
+pkgrev="1"
+pkgsuffix="git+bsos"
 DIST="brewmaster"
 urgency="low"
 maintainer="ProfessorKaos64"
@@ -108,34 +108,27 @@ main()
 	echo -e "\n==> Fetching upstream source\n"
 
 	# Get upstream source
-	git clone -b "v${pkgver}" "$git_url" "$git_dir"
-
-	# copy in debian folder
-	cp -r ""$scriptdir/debian"" "${git_dir}"
+	git clone -b "v${pkgver}" "${git_url}" "${git_dir}"
 
 	#################################################
 	# Build cmake source
 	#################################################
 
-
 	echo -e "\n==> Creating original tarball\n"sleep 2s
 	sleep 2s
 
-	# create the tarball from latest tarball creation script
-	# use latest revision designated at the top of this script
-
 	# create source tarball
+	cd "${build_dir}"
 	tar -cvzf "${pkgname}_${pkgver}.orig.tar.gz" "${src_dir}"
 
 	# copy in debian folder
 	cp -r "$scriptdir/debian" "${git_dir}"
 
 	# enter source dir
-	cd "${src_dir}"
+	cd "${git_dir}"
 
 	# gather commits
 	commits_full=$(git log --pretty=format:"  * %cd %h %s")
-
 
 	echo -e "\n==> Updating changelog"
 	sleep 2s
@@ -143,14 +136,16 @@ main()
  	# update changelog with dch
 	if [[ -f "debian/changelog" ]]; then
 
-		dch -p --force-distribution -v "${pkgver}+${pkgsuffix}" --package "${pkgname}" -D "${DIST}" -u "${urgency}"
+		dch -p --force-distribution -v "${pkgver}+${pkgsuffix}-${pkgrev}" --package "${pkgname}" \
+		-D "${DIST}" -u "${urgency}" "Initial upload"
+		nano "debian/changelog"
 
 	else
 
-		dch -p --create --force-distribution -v "${pkgver}+${pkgsuffix}" --package "${pkgname}" -D "${DIST}" -u "${urgency}"
+		dch -p --create --force-distribution -v "${pkgver}+${pkgsuffix}-${pkgrev}" --package "${pkgname}" \
+		-D "${DIST}" -u "${urgency}" "Initial upload"
 
 	fi
-
 
 	#################################################
 	# Build Debian package
@@ -166,37 +161,24 @@ main()
 	# Cleanup
 	#################################################
 
-	# clean up dirs
-
 	# note time ended
 	time_end=$(date +%s)
 	time_stamp_end=(`date +"%T"`)
 	runtime=$(echo "scale=2; ($time_end-$time_start) / 60 " | bc)
 
-	# output finish
-	echo -e "\nTime started: ${time_stamp_start}"
-	echo -e "Time started: ${time_stamp_end}"
-	echo -e "Total Runtime (minutes): $runtime\n"
-
-
-	# assign value to build folder for exit warning below
-	build_folder=$(ls -l | grep "^d" | cut -d ' ' -f12)
-
-	# back out of build temp to script dir if called from git clone
-	if [[ "${scriptdir}" != "" ]]; then
-		cd "${scriptdir}"
-	else
-		cd "${HOME}"
-	fi
-
 	# inform user of packages
-	echo -e "\n############################################################"
-	echo -e "If package was built without errors you will see it below."
-	echo -e "If you don't, please check build dependcy errors listed above."
-	echo -e "############################################################\n"
+	cat<<-EOF
 	
-	echo -e "Showing contents of: ${build_dir}: \n"
-	ls "${build_dir}" | grep $pkgname_$pkgver
+	###############################################################
+	If package was built without errors you will see it below.
+	If you don't, please check build dependcy errors listed above.
+	###############################################################
+	
+	Showing contents of: ${build_dir}
+	
+	EOF
+
+	ls "${build_dir}" | grep -E "${pkgver}" 
 
 	echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
 	sleep 0.5s
@@ -207,9 +189,10 @@ main()
 
 		# transfer files
 		if [[ -d "${build_dir}" ]]; then
-			rsync -arv -e "ssh -p ${REMOTE_PORT}" --filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" \
-			${build_dir}/ ${REMOTE_USER}@${REMOTE_HOST}:${REPO_FOLDER}
+			rsync -arv --filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" ${build_dir}/ ${USER}@${HOST}:${REPO_FOLDER}
 
+			# Keep changelog
+			cp "${git_dir}/debian/changelog" "${scriptdir}/debian/"
 		fi
 
 	elif [[ "$transfer_choice" == "n" ]]; then
@@ -219,12 +202,4 @@ main()
 }
 
 # start main
-
-	if [[ "${BUILDER}" != "pdebuild" ]]; then
-
-		# handle prereqs on host machine
-		install_prereqs
-
-	fi
-
 main

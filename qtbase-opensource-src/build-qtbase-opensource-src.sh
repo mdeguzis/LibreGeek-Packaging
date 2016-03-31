70bfe9f0127dd4ad59ba6f88e53b284ffb184312
mikeyd <mdeguzis@gmail.com>
2016-03-31 08:59:16 -0400

    pull repo if desired, since repo is large

--- qtbase-opensource-src/build-qtbase-opensource-src.sh
+++ qtbase-opensource-src/build-qtbase-opensource-src.sh
@@ -148,7 +148,50 @@ main()
 	echo -e "\n==> Obtaining upstream source code\n"
 	sleep 2s
 
-	git clone -b ${branch} ${git_url} ${git_dir}
+if [[ -d "${git_dir}" ]]; then
+
+		echo -e "\n==Info==\nGit folder already exists! Reclone [r] or pull [p]?\n"
+		sleep 1s
+		read -ep "Choice: " git_choice
+
+		if [[ "$git_choice" == "p" ]]; then
+			# attempt to pull the latest source first
+			echo -e "\n==> Attempting git pull..."
+			sleep 2s
+
+			# attempt git pull, if it doesn't complete reclone
+			if ! git pull; then
+
+				# command failure
+				echo -e "\n==Info==\nGit directory pull failed. Removing and cloning...\n"
+				sleep 2s
+				rm -rf "${git_dir}"
+				git clone -b "${branch}" "${git_url}" "${git_dir}"
+
+			fi
+
+		elif [[ "$git_choice" == "r" ]]; then
+			echo -e "\n==> Removing and cloning repository again...\n"
+			sleep 2s
+			sudo rm -rf "${git_dir}"
+			git clone -b "${branch}" "${git_url}" "${git_dir}"
+
+		else
+
+			echo -e "\n==> Git directory does not exist. cloning now...\n"
+			sleep 2s
+			git clone -b "${branch}" "${git_url}" "${git_dir}"
+
+		fi
+
+	else
+
+			echo -e "\n==> Git directory does not exist. cloning now...\n"
+			sleep 2s
+			# create and clone to current dir
+			git clone -b "${branch}" "${git_url}" "${git_dir}"
+
+	fi
 
 	# trim git (after confimed working build)
 	# rm -rf "${git_dir}/.git"

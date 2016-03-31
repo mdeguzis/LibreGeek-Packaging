e4cd6b12e9ef6cfa833d86433080769b46ac50c1
mikeyd <mdeguzis@gmail.com>
2016-03-31 10:51:28 -0400

    Update build-qtbase-opensource-src.sh

--- qtbase-opensource-src/build-qtbase-opensource-src.sh
+++ qtbase-opensource-src/build-qtbase-opensource-src.sh
@@ -135,40 +135,15 @@ main()
 
 	if [[ -d "${git_dir}" ]]; then
 
-		echo -e "\n==Info==\nGit folder already exists! Reclone [r] or pull [p]?\n"
+		echo -e "\n==Info==\nGit folder already exists! Remove and Reclone [r] ?\n"
 		sleep 1s
 		read -ep "Choice: " git_choice
 
-		if [[ "$git_choice" == "p" ]]; then
-
-			# attempt to pull the latest source first
-			echo -e "\n==> Attempting git pull..."
-			cd "${git_dir}" && git pull
-			sleep 2s
-
-			# attempt git pull, if it doesn't complete reclone
-			if ! git pull; then
-
-				# command failure
-				echo -e "\n==Info==\nGit directory pull failed. Removing and cloning...\n"
-				sleep 2s
-				sudo rm -rf "${build_dir}" && mkdir -p "${build_dir}"
-				git clone -b "${branch}" "${git_url}" "${git_dir}"
-
-			fi
-
-		elif [[ "$git_choice" == "r" ]]; then
+		if [[ "$git_choice" == "r" ]]; then
 
 			echo -e "\n==> Removing and cloning repository again...\n"
 			sleep 2s
 			sudo rm -rf "${build_dir}" && mkdir -p "${build_dir}"
-			git clone -b "${branch}" "${git_url}" "${git_dir}"
-
-		else
-
-			echo -e "\n==> Git directory does not exist. cloning now...\n"
-			sleep 2s
-			mkdir -p "${build_dir}"
 			git clone -b "${branch}" "${git_url}" "${git_dir}"
 
 		fi

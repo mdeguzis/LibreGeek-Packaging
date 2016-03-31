b39dca21c74786bd8af7a68a08a9bd78a7c54b48
mikeyd <mdeguzis@gmail.com>
2016-03-31 10:34:53 -0400

    Update configure-test.sh

--- qtbase-opensource-src/configure-test.sh
+++ qtbase-opensource-src/configure-test.sh
@@ -25,55 +25,52 @@ platform_arg="linux-g++-64"
 # Set number of jobs explicitly
 NB_CORES=$(grep -c '^processor' /proc/cpuinfo)
 
-%:
-	dh $@ --parallel --with pkgkde_symbolshelper
 
-override_dh_auto_configure:
-	MAKEFLAGS="-j$((NB_CORES+1)) -l${NB_CORES}" ./configure \
-			-confirm-license \
-			-prefix "/usr" \
-			-bindir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/bin" \
-			-libdir "/usr/lib/$(DEB_HOST_MULTIARCH)" \
-			-docdir "/usr/share/qt5/doc" \
-			-headerdir "/usr/include/$(DEB_HOST_MULTIARCH)/qt5" \
-			-datadir "/usr/share/qt5" \
-			-archdatadir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5" \
-			-hostdatadir "/usr/share/qt5" \
-			-plugindir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/plugins" \
-			-importdir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/imports" \
-			-translationdir "/usr/share/qt5/translations" \
-			-hostdatadir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5" \
-			-sysconfdir "/etc/xdg" \
-			-examplesdir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/examples" \
-			-opensource \
-			-system-sqlite \
-			-platform $(platform_arg) \
-			-plugin-sql-mysql
-			-plugin-sql-odbc
-			-plugin-sql-psql
-			-plugin-sql-sqlite
-			-no-sql-sqlite2
-			-plugin-sql-tds
-			-system-zlib \
-			-system-libpng \
-			-system-libjpeg \
-			-openssl \
-			-no-rpath \
-			-verbose \
-			-optimized-qmake \
-			-dbus-linked \
-			-no-strip \
-			-no-separate-debug-info \
-			-qpa xcb \
-			-xcb \
-			-glib \
-			-icu \
-			-accessibility \
-			-compile-examples \
-			-no-directfb \
-			-gstreamer 1.0 \
-			-no-sql-ibase -no-eglfs -opengl desktop \
-			$(cpu_opt)
+./configure \
+	-confirm-license \
+	-prefix "/usr" \
+	-bindir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/bin" \
+	-libdir "/usr/lib/$(DEB_HOST_MULTIARCH)" \
+	-docdir "/usr/share/qt5/doc" \
+	-headerdir "/usr/include/$(DEB_HOST_MULTIARCH)/qt5" \
+	-datadir "/usr/share/qt5" \
+	-archdatadir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5" \
+	-hostdatadir "/usr/share/qt5" \
+	-plugindir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/plugins" \
+	-importdir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/imports" \
+	-translationdir "/usr/share/qt5/translations" \
+	-hostdatadir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5" \
+	-sysconfdir "/etc/xdg" \
+	-examplesdir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/examples" \
+	-opensource \
+	-system-sqlite \
+	-platform $(platform_arg) \
+	-plugin-sql-mysql
+	-plugin-sql-odbc
+	-plugin-sql-psql
+	-plugin-sql-sqlite
+	-no-sql-sqlite2
+	-plugin-sql-tds
+	-system-zlib \
+	-system-libpng \
+	-system-libjpeg \
+	-openssl \
+	-no-rpath \
+	-verbose \
+	-optimized-qmake \
+	-dbus-linked \
+	-no-strip \
+	-no-separate-debug-info \
+	-qpa xcb \
+	-xcb \
+	-glib \
+	-icu \
+	-accessibility \
+	-compile-examples \
+	-no-directfb \
+	-gstreamer 1.0 \
+	-no-sql-ibase -no-eglfs -opengl desktop \
+	$(cpu_opt)
 
 # Retrun to top dir
 cd "${TOP}"

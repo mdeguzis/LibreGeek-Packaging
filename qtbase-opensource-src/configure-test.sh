28ffcb03ce60e2773102232f3f19e42caf07ce75
mikeyd <mdeguzis@gmail.com>
2016-03-31 10:18:04 -0400

    Update configure-test.sh

--- qtbase-opensource-src/configure-test.sh
+++ qtbase-opensource-src/configure-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # This assumes we have a "qt5" directory under home that is initialized
 # It is only meant for simple checks on configure options
@@ -7,108 +7,137 @@
 # Passes as of 20160328 commit 26bb70d
 ####################################################
 
-DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
-DEB_HOST_ARCH=$(dpkg-architecture -qDEB_HOST_ARCH)
-DEB_HOST_ARCH_OS=$(dpkg-architecture -qDEB_HOST_ARCH_OS)
-DEB_HOST_ARCH_BITS=$(dpkg-architecture -qDEB_HOST_ARCH_BITS)
-DEB_HOST_ARCH_CPU=$(dpkg-architecture -qDEB_HOST_ARCH_CPU)
+# Uncomment this to turn on verbose mode.
+#export DH_VERBOSE=1
 
-platform_arg="linux-g++-64"
+DEB_HOST_MULTIARCH ?= $(shell dpkg-architecture -qDEB_HOST_MULTIARCH)
+DEB_HOST_ARCH ?= $(shell dpkg-architecture -qDEB_HOST_ARCH)
+DEB_HOST_ARCH_OS ?= $(shell dpkg-architecture -qDEB_HOST_ARCH_OS)
+DEB_HOST_ARCH_BITS ?= $(shell dpkg-architecture -qDEB_HOST_ARCH_BITS)
+DEB_HOST_ARCH_CPU ?= $(shell dpkg-architecture -qDEB_HOST_ARCH_CPU)
 
-################################################################################
-# Checking a build log, there two are added at the end via $(extra_configure_opts)
-# (normally determined via dh/shell checks)
-################################################################################
+export PATH := $(PATH):$(shell pwd)/bin
+export CFLAGS := $(shell dpkg-buildflags --get CFLAGS) $(shell dpkg-buildflags --get CPPFLAGS)
+export CXXFLAGS := $(shell dpkg-buildflags --get CXXFLAGS) $(shell dpkg-buildflags --get CPPFLAGS)
+export LDFLAGS := $(shell dpkg-buildflags --get LDFLAGS) -Wl,--as-needed
 
-#-plugin-sql-ibase \
-#-opengl desktop
+# Upstream changelog
+upstream_changes := dist/changes-$(shell dpkg-parsechangelog | sed -n 's/^Version: //p' | cut -f1 -d '-' | sed -e 's/+dfsg//' | sed -e 's/+git.*//')
+# Distribution vendor
+vendor := $(shell dpkg-vendor --query Vendor)
 
-################################################################################
-# Currently disabled options from upstream Debian experimental debian/rules:
-################################################################################
+# To easier the files installation according to OSes and CPUs, we create three
+# kinds of install files: foo.install-common, foo.install-$DEB_HOST_ARCH_CPU and
+# foo.install-$DEB_HOST_ARCH_OS. In this case we can fine-tune what we install.
+#
+# Note that if any foo.install-* file exists and foo.install exist too, the
+# later will get overwritten.
+#
+# I've opened a bug in debhelper to allow this:
+# http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=703201
 
-#-plugin-sql-mysql \
-#-plugin-sql-odbc \
-#-plugin-sql-psql \
-#-plugin-sql-sqlite \
-#-no-sql-sqlite2 \
-#-plugin-sql-tds \
-#-system-sqlite \
-#-plugin-sql-ibase \
+# Retrieve packages that have a .install-common file
+pkgs_with_common = $(patsubst debian/%.install-common,%,$(wildcard debian/*.install-common))
+# Retrieve packages that have a .install-$DEB_HOST_ARCH_CPU file
+pkgs_with_arch = $(patsubst debian/%.install-$(DEB_HOST_ARCH_CPU),%,$(wildcard debian/*.install-$(DEB_HOST_ARCH_CPU)))
+# Retrieve packages that have a .install-$DEB_HOST_ARCH_OS file
+pkgs_with_os = $(patsubst debian/%.install-$(DEB_HOST_ARCH_OS),%,$(wildcard debian/*.install-$(DEB_HOST_ARCH_OS)))
 
-################################################################################
-# Taken out because the jessie/jessie-backport version is too old
-################################################################################
 
-#-system-harfbuzz \
+#ifneq (,$(filter libqt5sql5-ibase,$(shell dh_listpackages)))
+#	extra_configure_opts += -plugin-sql-ibase
+#else
+	extra_configure_opts += -no-sql-ibase
+#endif
 
-################################################################################
+no_pch_architectures := arm64
+ifeq ($(DEB_HOST_ARCH),$(findstring $(DEB_HOST_ARCH), $(no_pch_architectures)))
+	extra_configure_opts += -no-pch
+endif
 
-qtdir="$HOME/qt5"
-currdir="${PWD}"
-
-if [[ -d "${qtdir}" ]]; then
-
-	cd $HOME/qt5
-	./init-repository
-	
+gles2_architectures := armel armhf
+ifeq ($(DEB_HOST_ARCH),$(findstring $(DEB_HOST_ARCH), $(gles2_architectures)))
+	extra_configure_opts += -opengl es2
 else
+	extra_configure_opts += -opengl desktop
+endif
 
-	echo "Error! - $qtdir not found!"
-	exit 1
-	
-fi
+ifneq ($(DEB_HOST_ARCH_OS),linux)
+	extra_configure_opts += -no-eglfs
+endif
 
-./configure \
--confirm-license \
--prefix "/usr" \
--bindir "/usr/lib/$DEB_HOST_MULTIARCH/qt5/bin" \
--libdir "/usr/lib/$DEB_HOST_MULTIARCH" \
--docdir "/usr/share/qt5/doc" \
--headerdir "/usr/include/$DEB_HOST_MULTIARCH/qt5" \
--datadir "/usr/share/qt5" \
--archdatadir "/usr/lib/$DEB_HOST_MULTIARCH/qt5" \
--hostdatadir "/usr/share/qt5" \
--plugindir "/usr/lib/$DEB_HOST_MULTIARCH/qt5/plugins" \
--importdir "/usr/lib/$DEB_HOST_MULTIARCH/qt5/imports" \
--translationdir "/usr/share/qt5/translations" \
--hostdatadir "/usr/lib/$DEB_HOST_MULTIARCH/qt5" \
--sysconfdir "/etc/xdg" \
--examplesdir "/usr/lib/$DEB_HOST_MULTIARCH/qt5/examples" \
--opensource \
--platform $platform_arg \
--system-zlib \
--system-libpng \
--system-libjpeg \
--openssl \
--no-rpath \
--verbose \
--optimized-qmake \
--dbus-linked \
--no-strip \
--no-separate-debug-info \
--nomake examples \
--nomake tests \
--qpa xcb \
--xcb \
--glib \
--icu \
--accessibility \
--compile-examples \
--no-directfb \
--gstreamer 1.0 \
--opengl desktop \
--no-sql-ibase
+# Compile without sse2 support on i386
+# Do not use pre compiled headers in order to be able to rebuild the gui
+# submodule.
+ifeq ($(DEB_HOST_ARCH_CPU),i386)
+	cpu_opt = -no-sse2 -no-pch
+endif
 
-echo -e "\nReview log?"
-read -erp "Choice [y/n]: " choice
+ifeq ($(DEB_HOST_ARCH_OS),linux)
+  ifneq (,$(filter $(DEB_HOST_ARCH),alpha ia64 mips64 mips64el arm64))
+	platform_arg = linux-g++
+  else ifeq ($(DEB_HOST_ARCH_BITS),64)
+	platform_arg = linux-g++-64
+  else
+	platform_arg = linux-g++
+  endif
+else ifeq ($(DEB_HOST_ARCH_OS),hurd)
+	platform_arg = hurd-g++
+else ifeq ($(DEB_HOST_ARCH_OS),kfreebsd)
+	platform_arg = gnukfreebsd-g++
+else
+	$(error Unknown qmake mkspec for $(DEB_HOST_ARCH_OS))
+endif
 
-if [[ "${choice}" == "y" ]]; then
+# Set number of jobs explicitly
+NB_CORES ?= $(shell grep -c '^processor' /proc/cpuinfo)
 
-	less qt-configure-test.log
-	
-fi
+%:
+	dh $@ --parallel --with pkgkde_symbolshelper
 
-# clean up
-rm -f qt-configure-test.log
-cd "$currdir"
+override_dh_auto_configure:
+	MAKEFLAGS="-j$((NB_CORES+1)) -l${NB_CORES}" ./configure \
+			-confirm-license \
+			-prefix "/usr" \
+			-bindir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/bin" \
+			-libdir "/usr/lib/$(DEB_HOST_MULTIARCH)" \
+			-docdir "/usr/share/qt5/doc" \
+			-headerdir "/usr/include/$(DEB_HOST_MULTIARCH)/qt5" \
+			-datadir "/usr/share/qt5" \
+			-archdatadir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5" \
+			-hostdatadir "/usr/share/qt5" \
+			-plugindir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/plugins" \
+			-importdir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/imports" \
+			-translationdir "/usr/share/qt5/translations" \
+			-hostdatadir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5" \
+			-sysconfdir "/etc/xdg" \
+			-examplesdir "/usr/lib/$(DEB_HOST_MULTIARCH)/qt5/examples" \
+			-opensource \
+			-system-sqlite \
+			-platform $(platform_arg) \
+			-plugin-sql-mysql
+			-plugin-sql-odbc
+			-plugin-sql-psql
+			-plugin-sql-sqlite
+			-no-sql-sqlite2
+			-plugin-sql-tds
+			-system-zlib \
+			-system-libpng \
+			-system-libjpeg \
+			-openssl \
+			-no-rpath \
+			-verbose \
+			-optimized-qmake \
+			-dbus-linked \
+			-no-strip \
+			-no-separate-debug-info \
+			-qpa xcb \
+			-xcb \
+			-glib \
+			-icu \
+			-accessibility \
+			-compile-examples \
+			-no-directfb \
+			-gstreamer 1.0 \
+			$(extra_configure_opts) \
+			$(cpu_opt)

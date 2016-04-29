dab172d579db28e6c7e0f3297ae8abef1a887def
professorkaos64 <mdeguzis@gmail.com>
2016-04-29 09:44:54 -0400

    libtimefrane packaging

--- libframetime/build-godot.sh
+++ libframetime/build-godot.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# -------------------------------------------------------------------------------
-# Author:    	  Michael DeGuzis
-# Git:	    	  https://github.com/ProfessorKaos64/SteamOS-Tools
-# Scipt Name:	  build-laser-tank.sh
-# Script Ver:	  0.1.1
-# Description:	  Attempts to build a deb package from laser-tank git source
-# See:		  https://github.com/h4tr3d/laser-tank
-# Usage:	  ./build-laser-tank.sh
-# Opts:		  [--testing]
-#		  Modifys build script to denote this is a test package build.
+#-------------------------------------------------------------------------------
+# Author:	Michael DeGuzis
+# Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	build-godot.sh
+# Script Ver:	0.5.1
+# Description:	Attempts to build a deb package from latest godot
+#		github release
+#
+# See:		https://github.com/godotengine/godot
+#
+# Usage:	build-.sh
+# Opts:		[--testing]
+#		Modifys build script to denote this is a test package build.
 # -------------------------------------------------------------------------------
 
 #################################################
@@ -33,6 +36,8 @@ if [[ "${REMOTE_USER}" == "" || "${REMOTE_HOST}" == "" ]]; then
 
 fi
 
+
+
 if [[ "$arg1" == "--testing" ]]; then
 
 	REPO_FOLDER="/home/mikeyd/packaging/SteamOS-Tools/incoming_testing"
@@ -43,53 +48,50 @@ else
 
 fi
 
-# upstream URL
-git_url="https://github.com/h4tr3d/laser-tank"
-branch="master"
+# upstream vars
+git_url="https://github.com/godotengine/godot"
+branch="2.0-stable"
+
 
 # package vars
 date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
 date_short=$(date +%Y%m%d)
 ARCH="amd64"
 BUILDER="pdebuild"
-BUILDOPTS=""
-export USE_NETWORK="no"
-export STEAMOS_TOOLS_BETA_HOOK="true"
-uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
-pkgname="laser-tank"
-pkgver="0.${date_short}"
-BUILDER="pdebuild"
+BUILDOPTS="--debbuildopts -b"
 export STEAMOS_TOOLS_BETA_HOOK="false"
+export USE_NETWORK="no"
+pkgname="godot"
+pkgver="2.0"
+upstream_rev="1"
 pkgrev="1"
-pkgsuffix="git+bsos"
+pkgsuffix="bsos${pkgrev}"
 DIST="brewmaster"
 urgency="low"
+uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
 maintainer="ProfessorKaos64"
 
-# set build directories
+# set build_dir
 export build_dir="${HOME}/build-${pkgname}-temp"
 src_dir="${pkgname}-${pkgver}"
 git_dir="${build_dir}/${src_dir}"
 
 install_prereqs()
 {
-
+	clear
 	echo -e "==> Installing prerequisites for building...\n"
 	sleep 2s
-	# install needed packages from Debian repos
-	sudo apt-get install -y --force-yes git devscripts build-essential libsdl2-dev \
-	libsdl2-ttf-dev cmake g++
+	# install basic build packages
+	sudo apt-get -y --force-yes install autoconf automake build-essential bc debhelper \
+ 	gcc python scons libx11-dev pkg-config libxcursor-dev libasound2-dev libfreetype6-dev \
+ 	libgl1-mesa-dev libglu-dev libssl-dev libxinerama-dev libudev-dev
 
 }
 
 main()
 {
 
-	#################################################
-	# Fetch source
-	#################################################
-
-	# create and enter build_dir
+	# create build_dir
 	if [[ -d "${build_dir}" ]]; then
 
 		sudo rm -rf "${build_dir}"
@@ -101,10 +103,11 @@ main()
 
 	fi
 
-	# Enter build dir
-	cd "${build_dir}"
+	# enter build dir
+	cd "${build_dir}" || exit
 
 	# install prereqs for build
+
 	if [[ "${BUILDER}" != "pdebuild" ]]; then
 
 		# handle prereqs on host machine
@@ -112,24 +115,18 @@ main()
 
 	fi
 
-	#################################################
-	# Fetch PMP source
-	#################################################
+	# Clone upstream source code and branch
 
 	echo -e "\n==> Obtaining upstream source code\n"
 
-	git clone -b "${branch}" "${git_url}" "${git_dir}"
-	cd "${git_dir}"
-	latest_commit=$(git log -n 1 --pretty=format:"%h")
+	# clone
+	git clone  -b "${branch}" "${git_url}" "${git_dir}"
 
-	# Add launcher script
-	cp -r "${scriptdir}/laser-tank.sh" "${git_dir}/laser-tank"
-
-	# enter git dir
-	cd "${git_dir}"
+	# Add art / other files
+	cp "${scriptdir}/godot.png" "${git_dir}"
 
 	#################################################
-	# Build package source
+	# Build package
 	#################################################
 
 	echo -e "\n==> Creating original tarball\n"
@@ -139,35 +136,38 @@ main()
 	cd "${build_dir}"
 	tar -cvzf "${pkgname}_${pkgver}+${pkgsuffix}.orig.tar.gz" "${src_dir}"
 
-	# copy in debian folder and other files
-        cp -r "${scriptdir}/debian" "${git_dir}"
+	# Add debian dir
+	cp -r "${scriptdir}/debian" "${git_dir}"
 
 	# enter source dir
 	cd "${git_dir}"
 
-	commits_full=$(git log --pretty=format:"  * %cd %h %s")
-
 	echo -e "\n==> Updating changelog"
 	sleep 2s
 
- 	# update changelog with dch
+	# Create basic changelog format if it does exist or update
 	if [[ -f "debian/changelog" ]]; then
 
-		dch -p --force-distribution -v "${pkgver}+${pkgsuffix}-${pkgrev}" --package \
-		"${pkgname}" -D "${DIST}" -u "${urgency}" "Update to latest commit [${latest_commit}]"
+		dch -p --force-distribution -v "${pkgver}+${pkgsuffix}-${upstream_rev}" --package "${pkgname}" -D $DIST -u "${urgency}" \
+		"Initial upload attempt"
 		nano "debian/changelog"
 
 	else
 
-		dch -p --create --force-distribution -v "${pkgver}+${pkgsuffix}-${pkgrev}" --package \
-		"${pkgname}" -D "${DIST}" -u "${urgency}" "Initial upload"
+		dch -p --force-distribution --create -v "${pkgver}+${pkgsuffix}-${upstream_rev}" --package "${pkgname}" -D "${DIST}" \
+		-u "${urgency}" "Initial upload attempt"
 		nano "debian/changelog"
 
 	fi
 
-	echo -e "\n==> Building Debian package from source\n"
+	#################################################
+	# Build Debian package
+	#################################################
+
+	echo -e "\n==> Building Debian package ${pkgname} from source\n"
 	sleep 2s
 
+	#  build
 	DIST=$DIST ARCH=$ARCH ${BUILDER} ${BUILDOPTS}
 
 	#################################################
@@ -210,8 +210,6 @@ main()
 			rsync -arv --info=progress2 -e "ssh -p ${REMOTE_PORT}" --filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" \
 			${build_dir}/ ${REMOTE_USER}@${REMOTE_HOST}:${REPO_FOLDER}
 
-			# Keep changelog
-			cp "${git_dir}/debian/changelog" "${scriptdir}/debian/"
 		fi
 
 	elif [[ "$transfer_choice" == "n" ]]; then

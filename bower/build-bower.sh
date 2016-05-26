79c99619da3cd3e0a69104a113a2728eeea72255
professorkaos64 <mdeguzis@gmail.com>
2016-05-26 12:27:03 -0400

    work on some deps for twitch gui app

---
+++ bower/build-bower.sh
@@ -0,0 +1,226 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+# Author:	Michael DeGuzis
+# Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	build-bower.sh
+# Script Ver:	0.1.1
+# Description:	Builds simple pacakge for using bower based of of master upstream
+#		git source
+#
+# See:		https://github.com/ProfessorKaos64/bower/
+#
+# Usage:	./build-bower.sh
+# Opts:		[--testing]
+#		Modifys build script to denote this is a test package build.
+# -------------------------------------------------------------------------------
+
+#################################################
+# Set variables
+#################################################
+
+arg1="$1"
+scriptdir=$(pwd)
+time_start=$(date +%s)
+time_stamp_start=(`date +"%T"`)
+
+
+# Check if USER/HOST is setup under ~/.bashrc, set to default if blank
+# This keeps the IP of the remote VPS out of the build script
+
+if [[ "${REMOTE_USER}" == "" || "${REMOTE_HOST}" == "" ]]; then
+
+	# fallback to local repo pool target(s)
+	REMOTE_USER="mikeyd"
+	REMOTE_HOST="archboxmtd"
+	REMOTE_PORT="22"
+
+fi
+
+
+
+if [[ "$arg1" == "--testing" ]]; then
+
+	REPO_FOLDER="/home/mikeyd/packaging/SteamOS-Tools/incoming_testing"
+
+else
+
+	REPO_FOLDER="/home/mikeyd/packaging/SteamOS-Tools/incoming"
+
+fi
+
+# upstream vars
+git_url="https://github.com/ProfessorKaos64/bower"
+rel_target="v1.7.6"
+
+# package vars
+date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
+date_short=$(date +%Y%m%d)
+ARCH="amd64"
+BUILDER="pdebuild"
+BUILDOPTS=""
+export STEAMOS_TOOLS_BETA_HOOK="false"
+pkgname="node-bower"
+pkgver="0.7.6"
+upstream_rev="1"
+pkgrev="1"
+pkgsuffix="bsos"
+DIST="brewmaster"
+urgency="low"
+uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
+maintainer="ProfessorKaos64"
+
+# set build_dir
+export build_dir="${HOME}/build-${pkgname}-temp"
+src_dir="${pkgname}-${pkgver}"
+git_dir="${build_dir}/${src_dir}"
+
+install_prereqs()
+{
+	clear
+	echo -e "==> Installing prerequisites for building...\n"
+	sleep 2s
+
+	# Avoid libattr garbage for 32 bit package installed by emulators
+	if [[ -f "/usr/share/doc/libattr1/changelog.Debian.gz" ]]; then
+
+		sudo mv "/usr/share/doc/libattr1/changelog.Debian.gz" \
+		"/usr/share/doc/libattr1/changelog.Debian.gz.old" 2> /dev/null
+	fi
+
+	# install basic build packages
+	# Additional suggested packages added per: https://wiki.debian.org/Python/LibraryStyleGuide
+	sudo apt-get install -y --force-yes build-essential bc  debhelper dh-buildinfo nodejs
+
+}
+
+main()
+{
+
+	# create build_dir
+	if [[ -d "${build_dir}" ]]; then
+
+		sudo rm -rf "${build_dir}"
+		mkdir -p "${build_dir}"
+
+	else
+
+		mkdir -p "${build_dir}"
+
+	fi
+
+	# enter build dir
+	cd "${build_dir}" || exit
+
+	# install prereqs for build
+
+	if [[ "${BUILDER}" != "pdebuild" ]]; then
+
+		# handle prereqs on host machine
+		install_prereqs
+
+	fi
+
+
+	echo -e "\n==> Obtaining upstream source code\n"
+
+	# clone and checkout desired commit
+	git clone -b "$rel_target" "$git_url" "${git_dir}"
+
+	#################################################
+	# Build package
+	#################################################
+
+	echo -e "\n==> Creating original tarball\n"
+	sleep 2s
+
+	# create source tarball
+	tar -cvzf "${pkgname}_${pkgver}+${pkgsuffix}.orig.tar.gz" "$pkgname"
+
+	# Enter git dir to build
+	cd "${git_dir}"
+
+
+	echo -e "\n==> Updating changelog"
+	sleep 2s
+
+ 	# update changelog with dch
+	if [[ -f "debian/changelog" ]]; then
+
+		dch -p --force-distribution -v "${pkgver}+${pkgsuffix}" --package "${pkgname}" -D "${DIST}" -u "${urgency}"
+
+	else
+
+		dch -p --create --force-distribution -v "${pkgver}+${pkgsuffix}" --package "${pkgname}" -D "${DIST}" -u "${urgency}"
+
+	fi
+
+
+	#################################################
+	# Build Debian package
+	#################################################
+
+	echo -e "\n==> Building Debian package ${pkgname} from source\n"
+	sleep 2s
+
+	DIST=$DIST ARCH=$ARCH ${BUILDER} ${BUILDOPTS}
+
+	#################################################
+	# Cleanup
+	#################################################
+
+	# note time ended
+	time_end=$(date +%s)
+	time_stamp_end=(`date +"%T"`)
+	runtime=$(echo "scale=2; ($time_end-$time_start) / 60 " | bc)
+
+	# output finish
+	echo -e "\nTime started: ${time_stamp_start}"
+	echo -e "Time started: ${time_stamp_end}"
+	echo -e "Total Runtime (minutes): $runtime\n"
+
+	# assign value to build folder for exit warning below
+	build_folder=$(ls -l | grep "^d" | cut -d ' ' -f12)
+
+	# back out of build temp to script dir if called from git clone
+	if [[ "${scriptdir}" != "" ]]; then
+		cd "${scriptdir}" || exit
+	else
+		cd "${HOME}" || exit
+	fi
+
+	# inform user of packages
+	echo -e "\n############################################################"
+	echo -e "If package was built without errors you will see it below."
+	echo -e "If you don't, please check build dependcy errors listed above."
+	echo -e "############################################################\n"
+
+	echo -e "Showing contents of: ${build_dir}: \n"
+	ls "${build_dir}" | grep ${pkgver}
+
+	if [[ "$autobuild" != "yes" ]]; then
+
+		echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
+		sleep 0.5s
+		# capture command
+		read -erp "Transfer files: [y/n] " transfer_choice
+
+		if [[ "$transfer_choice" == "y" ]]; then
+
+			# transfer packages
+			rsync -arv --info=progress2 -e "ssh -p ${REMOTE_PORT}" --filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" \
+			${build_dir}/ ${REMOTE_USER}@${REMOTE_HOST}:${REPO_FOLDER}
+
+
+			# Preserve changelog
+			cd "${git_dir}" && git add debian/changelog && git push origin master
+			cd "${scriptdir}"
+
+		elif [[ "$transfer_chice" == "n" ]]; then
+			echo -e "Upload not requested\n"
+		fi
+	fi
+
+}
+
+# start main
+main

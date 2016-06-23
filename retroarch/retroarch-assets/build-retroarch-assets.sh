f6044098ab5d4b59bed512ab0ee3c903a047c8f3
professorkaos64 <mdeguzis@gmail.com>
2016-06-23 10:58:33 -0400

    update pkg

--- retroarch/retroarch-assets/build-retroarch-assets.sh
+++ retroarch/retroarch-assets/build-retroarch-assets.sh
@@ -206,7 +206,8 @@ main()
 
 		# transfer files
 		if [[ -d "${build_dir}" ]]; then
-			rsync -arv --filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" \
+			rsync -arv --info=progress2 -e "ssh -p ${REMOTE_PORT}" \
+			--filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" \
 			${build_dir}/ ${REMOTE_USER}@${REMOTE_HOST}:${REPO_FOLDER}
 
 			# Keep changelog

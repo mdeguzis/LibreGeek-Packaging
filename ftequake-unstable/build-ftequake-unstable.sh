04004d6a4421e898670a050cde31fd343ef3636c
professorkaos64 <mdeguzis@gmail.com>
2016-07-28 14:42:16 -0400

    add ftequake unstable

---
+++ ftequake-unstable/build-ftequake-unstable.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+# Author:	Michael DeGuzis
+# Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt name:	build-ftequake.sh
+# Script Ver:	0.1.1
+# Description:	Attempts to build a deb package from the latest ftequake source
+#		code.
+#
+# See:		https://github.com/Novum/ftequake
+#
+# Usage:	./build-ftequake.sh
+# Opts:		[--testing]
+#		Modifys build script to denote this is a test package build.
+# -------------------------------------------------------------------------------
+
+#################################################
+# Set variables
+#################################################
+
+arg1="$1"
+scriptdir=$(pwd)
+time_start=$(date +%s)
+time_stamp_start=(`date +"%T"`)
+
+
+# Check if USER/HOST is setup under ~/.bashrc, set to default if blank
+# This keeps the IP of the remote VPS out of the build script
+
+if [[ "${REMOTE_USER}" == "" || "${REMOTE_HOST}" == "" ]]; then
+
+	# fallback to local repo pool target(s)
+	REMOTE_USER="mikeyd"
+	REMOTE_HOST="archboxmtd"
+	REMOTE_PORT="22"
+
+fi
+
+if [[ "$arg1" == "--testing" ]]; then
+
+	REPO_FOLDER="/home/mikeyd/packaging/steamos-tools/incoming_testing"
+
+else
+
+	REPO_FOLDER="/home/mikeyd/packaging/steamos-tools/incoming"
+
+fi
+# upstream vars
+SVN_URL="http://svn.code.sf.net/p/fteqw/code/trunk"
+SVN_REV="r5010"
+
+# package vars
+date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
+date_short=$(date +%Y%m%d)
+ARCH="amd64"
+BUILDER="pdebuild"
+BUILDOPTS=""
+PKGNAME="ftequake"
+PKGVER="0.1.0"
+PKGREV="1"
+PKGSUFFIX="${SVN_REV}svn+bsos"
+DIST="brewmaster"
+urgency="low"
+uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
+maintainer="ProfessorKaos64"
+
+# set build directories
+export BUILD_DIR="${HOME}/build-${PKGNAME}-temp"
+SRC_DIR="${PKGNAME}-${PKGVER}"
+SVN_DIR="${BUILD_DIR}/${SRC_DIR}"
+
+install_prereqs()
+{
+	clear
+	echo -e "==> Installing prerequisites for building...\n"
+	sleep 2s
+	# install basic build packages
+	sudo apt-get install -y --force-yes build-essential subversion
+
+}
+
+main()
+{
+
+	# create BUILD_DIR
+	if [[ -d "${BUILD_DIR}" ]]; then
+
+		sudo rm -rf "${BUILD_DIR}"
+		mkdir -p "${BUILD_DIR}"
+
+	else
+
+		mkdir -p "${BUILD_DIR}"
+
+	fi
+
+	# enter build dir
+	cd "${BUILD_DIR}" || exit
+
+	# install prereqs for build
+	if [[ "${BUILDER}" != "pdebuild" ]]; then
+
+		# handle prereqs on host machine
+		install_prereqs
+		
+	else
+	
+	  # still need subversion
+	  sudo apt-get install -y --force-yes subversion
+
+	fi
+
+	echo -e "\n==> Obtaining upstream source code\n"
+
+	# checkout desired revision
+	svn checkout -r "${SVN_REV}" "${SVN_URL}" "${SVN_DIR}"
+	
+	# Add extras
+	cp -r "${scriptdir}/ftequake.png" "${SVN_DIR}"
+
+	#################################################
+	# Build package
+	#################################################
+
+	echo -e "\n==> Creating original tarball\n"
+	sleep 2s
+
+	# create source tarball
+	cd "${BUILD_DIR}" || exit
+	tar -cvzf "${PKGNAME}_${PKGVER}+${PKGSUFFIX}.orig.tar.gz" "${SRC_DIR}"
+
+	# Add required debian files
+	cp -r "${scriptdir}/debian" "${SVN_DIR}"
+
+	# enter source dir
+	cd "${SVN_DIR}"
+
+	echo -e "\n==> Updating changelog"
+	sleep 2s
+
+	# update changelog with dch
+	if [[ -f "debian/changelog" ]]; then
+
+		dch -p --force-distribution -v "${PKGVER}+${PKGSUFFIX}-${PKGREV}" --package "${PKGNAME}" \
+		-D "${DIST}" -u "${urgency}" "Update to svn revision ${SVN_REV}"
+		nano "debian/changelog"
+	
+	else
+
+		dch -p --create --force-distribution -v "${PKGVER}+${PKGSUFFIX}-${PKGREV}" \
+		--package "${PKGNAME}" -D "${DIST}" -u "${urgency}" "Initial build, revision ${LATEST_REV}"
+		nano "debian/changelog"
+
+	fi
+
+	#################################################
+	# Build Debian package
+	#################################################
+
+	echo -e "\n==> Building Debian package ${PKGNAME} from source\n"
+	sleep 2s
+
+	USENETWORK=$NETWORK DIST=$DIST ARCH=$ARCH ${BUILDER} ${BUILDOPTS}
+
+	#################################################
+	# Cleanup
+	#################################################
+
+	# clean up dirs
+
+	# note time ended
+	time_end=$(date +%s)
+	time_stamp_end=(`date +"%T"`)
+	runtime=$(echo "scale=2; ($time_end-$time_start) / 60 " | bc)
+
+	# output finish
+	echo -e "\nTime started: ${time_stamp_start}"
+	echo -e "Time started: ${time_stamp_end}"
+	echo -e "Total Runtime (minutes): $runtime\n"
+
+
+	# assign value to build folder for exit warning below
+	build_folder=$(ls -l | grep "^d" | cut -d ' ' -f12)
+
+	# inform user of packages
+	cat<<- EOF
+	#################################################################
+	If package was built without errors you will see it below.
+	If you don't, please check build dependency errors listed above.
+	#################################################################
+
+	EOF
+
+	echo -e "Showing contents of: ${BUILD_DIR}: \n"
+	ls "${BUILD_DIR}" | grep -E *${PKGVER}*
+
+	echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
+	sleep 0.5s
+	# capture command
+	read -erp "Choice: " transfer_choice
+
+	if [[ "$transfer_choice" == "y" ]]; then
+
+		if [[ -d "${BUILD_DIR}" ]]; then
+
+			# copy files to remote server
+			rsync -arv --info=progress2 -e "ssh -p ${REMOTE_PORT}" \
+			--filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" \
+			${BUILD_DIR}/ ${REMOTE_USER}@${REMOTE_HOST}:${REPO_FOLDER}
+
+			# uplaod local repo changelog
+			cp "${SVN_DIR}/debian/changelog" "${scriptdir}/debian"
+
+		fi
+
+	elif [[ "$transfer_choice" == "n" ]]; then
+		echo -e "Upload not requested\n"
+	fi
+
+}
+
+# start main
+main

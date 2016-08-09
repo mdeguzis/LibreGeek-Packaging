ed692d85c120ab70eb7c71c9ddb593c9fd8ad1ac
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-08-09 12:16:38 -0400

    Update build-libretro-lutro.sh

--- retroarch/libretro-lutro/build-libretro-lutro.sh
+++ retroarch/libretro-lutro/build-libretro-lutro.sh
@@ -120,7 +120,7 @@ main()
 	cd "${GIT_DIR}"
 	latest_commit=$(git log -n 1 --pretty=format:"%h")
 	
-	# Get rid of test target in Makefile
+	# Disable test target in Makefile
 	# It attempts to run the core with Retorarch, in a CLI...?
 	sed -i 's/test: all/#test: all/' "${GIT_DIR}/Makefile"
 	sed -i 's/retroarch \-L lutro_libretro\.so test/#retroarch \-L lutro_libretro\.so test/' "${GIT_DIR}/Makefile"

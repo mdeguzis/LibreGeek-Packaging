af6e375b1e463837656b254f6fc585b980227067
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-08-11 10:33:51 -0400

    Update build-vkquake.sh

--- vkquake/build-vkquake.sh
+++ vkquake/build-vkquake.sh
@@ -54,7 +54,7 @@ DATE_LONG=$(date +"%a, %d %b %Y %H:%M:%S %z")
 DATE_SHORT=$(date +%Y%m%d)
 ARCH="amd64"
 BUILDER="sbuild"
-BUILDOPTS="-d jessie"
+BUILDOPTS="-d jessie -c jessie-amd64-sbuild"
 export STEAMOS_TOOLS_BETA_HOOK="false"
 PKGNAME="vkquake"
 # Source version from vkQuake/Quake/quakedef.h

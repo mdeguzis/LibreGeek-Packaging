e1c87890b145c425ea05cdef5f074422f2c232c4
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-08-12 15:49:43 -0400

    Update build-vulkan.sh

--- vulkan/build-vulkan.sh
+++ vulkan/build-vulkan.sh
@@ -131,10 +131,13 @@ main()
 	fi
 	
 	# Refresh what local debian has
-	echo -e "\n==> Refreshing SPIRV files"
-	sleep 2s
-	cp -rv "${GIT_DIR}/external/spirv-tools/include/spirv-tools/libspirv.h" "${SCRIPTDIR}/debian/SPIRV"
-	cp -rv "${GIT_DIR}/external/spirv-headers/include/spirv/1.1/libspirv.h" "${SCRIPTDIR}/debian/spirv-tools"
+	#echo -e "\n==> Refreshing SPIRV files"
+	#sleep 2s
+	#cp -rv "${GIT_DIR}/external/spirv-tools/include/spirv-tools/libspirv.h" "${SCRIPTDIR}/debian/SPIRV"
+	#cp -rv "${GIT_DIR}/external/spirv-headers/include/spirv/1.1/libspirv.h" "${SCRIPTDIR}/debian/spirv-tools"
+	
+	# remove the external directory so it does not conflict
+	#rm -rf "${GIT_DIR}/external"
 
 	#################################################
 	# Build platform

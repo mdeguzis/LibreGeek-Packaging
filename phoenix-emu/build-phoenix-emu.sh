26d881f4c73f967d015f0e037e320211bb55d247
professorkaos64 <mdeguzis@gmail.com>
2016-08-23 10:39:58 -0400

    start qtgamepad

--- phoenix-emu/build-phoenix-emu.sh
+++ phoenix-emu/build-phoenix-emu.sh
@@ -2,14 +2,14 @@
 #-------------------------------------------------------------------------------
 # Author:	Michael DeGuzis
 # Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
-# Scipt Name:	build-3doh.sh
+# Scipt Name:	build-phoenix-emu.sh
 # Script Ver:	1.0.0
-# Description:	Attmpts to build a deb package from latest 3doh
-#		github release
+# Description:	Builds simple pacakge for phoenix-emu (Libretro front-end)
+# See:		http://www.arts-union.ru/sites/default/files/ph27-lin-x64.zip
 #
-# See:		https://github.com/ProfessorKaos64/3doh
 #
-# Usage:	./build-3doh.sh
+# Usage:	./build-phoenix-emu.sh [option]
+# Options:	---recursive -build-test
 # Opts:		[--testing]
 #		Modifys build script to denote this is a test package build.
 # -------------------------------------------------------------------------------
@@ -47,37 +47,38 @@ else
 fi
 
 # upstream vars
-#SRC_URL="https://github.com/ProfessorKaos64/3doh"
-SRC_URL="https://github.com/arcadenea/3doh"
-TARGET="master"
+SRC_URL="http://www.arts-union.ru/sites/default/files/ph27-lin-x64.zip"
+TARGET="ph27"
 
 # package vars
 DATE_LONG=$(date +"%a, %d %b %Y %H:%M:%S %z")
 DATE_SHORT=$(date +%Y%m%d)
-ARCH="i386"
+ARCH="amd64"
 BUILDER="pdebuild"
-BUILDOPTS="--debbuildopts -nc"
-export STEAMOS_TOOLS_BETA_HOOK="false"
-PKGNAME="3doh"
-PKGVER="0.1.0"
+BUILDOPTS=""
+export STEAMOS_TOOLS_BETA_HOOK="true"
+PKGNAME="phoenix-emu"
+PKGVER="2.7"
 PKGREV="1"
-PKGSUFFIX="${DATE_SHORT}git+bsos"
+PKGSUFFIX="bsos"
 DIST="brewmaster"
 URGENCY="low"
 UPLOADER="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
 MAINTAINER="ProfessorKaos64"
 
 # set BUILD_TMP
-export export BUILD_TMP="${HOME}/build-${PKGNAME}-tmp"
-export SRC_DIR="${BUILD_TMP}/${PKGNAME}-${PKGVER}"
+export BUILD_TMP="${HOME}/build-${PKGNAME}-tmp"
+SRC_DIR="${BUILD_TMP}/${PKGNAME}-${PKGVER}"
 
 install_prereqs()
 {
 	clear
 	echo -e "==> Installing prerequisites for building...\n"
 	sleep 2s
+
+
 	# install basic build packages
-	sudo apt-get install -y --force-yes build-essential bc 
+	sudo apt-get install -y --force-yes build-essential
 
 }
 
@@ -100,7 +101,7 @@ main()
 	cd "${BUILD_TMP}" || exit
 
 	# install prereqs for build
-	
+
 	if [[ "${BUILDER}" != "pdebuild" && "${BUILDER}" != "sbuild" ]]; then
 
 		# handle prereqs on host machine
@@ -108,32 +109,31 @@ main()
 
 	fi
 
+	echo -e "\n==> Obtaining upstream source code\n"
 
-	# Clone upstream source code and branch
+	mkdir -p "${SRC_DIR}"
+	wget "http://www.arts-union.ru/sites/default/files/${TARGET}-lin-x64.zip" \
+	-q -nc --show-progress
+	unzip "${TARGET}-lin-x64.zip" -d "${SRC_DIR}"
 
-	echo -e "\n==> Obtaining upstream source code"
-
-	# clone
-	git clone -b "${TARGET}" "${SRC_URL}" "${SRC_DIR}"
+	# launcher
+	cp "${SCRIPTDIR}/phoenix-emu-launch" "${SRC_DIR}"
 
 	#################################################
-	# Build package
+	# Prep source
 	#################################################
 
-	echo -e "\n==> Creating original tarball"
-	sleep 2s
-
-	# Trim .git folders
-	find "${SRC_DIR}" -name ".git" -type d -exec sudo rm -r {} \;
-
-	# create source tarball
 	cd "${BUILD_TMP}"
 	tar -cvzf "${PKGNAME}_${PKGVER}+${PKGSUFFIX}.orig.tar.gz" $(basename ${SRC_DIR})
 
-	# copy in debian folder
+	# add debian/
 	cp -r "${SCRIPTDIR}/debian" "${SRC_DIR}"
 
-	# enter source dir
+	###############################################################
+	# build package
+	###############################################################
+
+	# Enter git dir to build
 	cd "${SRC_DIR}"
 
 	echo -e "\n==> Updating changelog"
@@ -143,8 +143,8 @@ main()
 	if [[ -f "debian/changelog" ]]; then
 
 		dch -p --force-distribution -v "${PKGVER}+${PKGSUFFIX}-${PKGREV}" --package "${PKGNAME}" \
-		-D "${DIST}" -u "${URGENCY}" "Update build"
-		nano "debian/control"
+		-D "${DIST}" -u "${URGENCY}" "Update release"
+		nano "debian/changelog"
 
 	else
 
@@ -162,34 +162,33 @@ main()
 	sleep 2s
 
 	DIST=$DIST ARCH=$ARCH ${BUILDER} ${BUILDOPTS}
-	
+
 	#################################################
 	# Cleanup
 	#################################################
-	
+
 	# clean up dirs
-	
+
 	# note time ended
 	time_end=$(date +%s)
 	time_stamp_end=(`date +"%T"`)
 	runtime=$(echo "scale=2; ($time_end-$TIME_START) / 60 " | bc)
-	
+
 	# output finish
 	echo -e "\nTime started: ${TIME_STAMP_START}"
 	echo -e "Time started: ${time_stamp_end}"
 	echo -e "Total Runtime (minutes): $runtime\n"
 
-	
 	# assign value to build folder for exit warning below
 	build_folder=$(ls -l | grep "^d" | cut -d ' ' -f12)
-	
+
 	# back out of build tmp to script dir if called from git clone
 	if [[ "${SCRIPTDIR}" != "" ]]; then
 		cd "${SCRIPTDIR}" || exit
 	else
 		cd "${HOME}" || exit
 	fi
-	
+
 	# inform user of packages
 	cat<<- EOF
 	#################################################################

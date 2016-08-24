32b7b7990a7e1a2ad08a323787cc0272a75b1115
professorkaos64 <mdeguzis@gmail.com>
2016-08-24 12:08:37 -0400

    start hnblebndlmgr build

---
+++ humblebundlemgr/build-humblebundlemgr.sh
@@ -0,0 +1,264 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+# Author:	Michael DeGuzis
+# Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	build-ice.sh
+# Script Ver:	1.9.5
+# Description:	Builds simple pacakge for using ice based of of master upstream
+#		git source
+#
+# See:		https://github.com/scottrice/Ice
+#
+# Usage:	./build-ice-steamos.sh
+# Opts:		[--testing]
+#		Modifys build script to denote this is a test package build.
+# -------------------------------------------------------------------------------
+
+#################################################
+# Set variables
+#################################################
+
+arg1="$1"
+SCRIPTDIR=$(pwd)
+TIME_START=$(date +%s)
+TIME_STAMP_START=(`date +"%T"`)
+
+
+# Check if USER/HOST is setup under ~/.bashrc, set to default if blank
+# This keeps the IP of the remote VPS out of the build script
+
+if [[ "${REMOTE_USER}" == "" || "${REMOTE_HOST}" == "" ]]; then
+
+	# fallback to local repo pool TARGET(s)
+	REMOTE_USER="mikeyd"
+	REMOTE_HOST="archboxmtd"
+	REMOTE_PORT="22"
+
+fi
+
+
+
+if [[ "$arg1" == "--testing" ]]; then
+
+	REPO_FOLDER="/home/mikeyd/packaging/steamos-tools/incoming_testing"
+	
+else
+
+	REPO_FOLDER="/home/mikeyd/packaging/steamos-tools/incoming"
+	
+fi
+
+# upstream vars
+GIT_URL="https://github.com/scottrice/Ice"
+rel_TARGET="1.0.0"
+
+# package vars
+DATE_LONG=$(date +"%a, %d %b %Y %H:%M:%S %z")
+DATE_SHORT=$(date +%Y%m%d)
+ARCH="amd64"
+BUILDER="pdebuild"
+BUILDOPTS="--debbuildopts -b"
+export STEAMOS_TOOLS_BETA_HOOK="false"
+PKGNAME="ice-steamos"
+PKGVER="1.0.0"
+upstream_rev="1"
+PKGREV="9"
+PKGSUFFIX="bsos${PKGREV}"
+DIST="brewmaster"
+URGENCY="low"
+UPLOADER="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
+MAINTAINER="ProfessorKaos64"
+
+# set BUILD_TMP
+export BUILD_TMP="${HOME}/build-${PKGNAME}-tmp"
+SRCDIR="${PKGNAME}-${PKGVER}"
+GIT_DIR="${BUILD_TMP}/${SRCDIR}"
+
+install_prereqs()
+{
+	clear
+	echo -e "==> Installing prerequisites for building...\n"
+	sleep 2s
+
+	# Avoid libattr garbage for 32 bit package installed by emulators
+	if [[ -f "/usr/share/doc/libattr1/changelog.Debian.gz" ]]; then
+
+		sudo mv "/usr/share/doc/libattr1/changelog.Debian.gz" \
+		"/usr/share/doc/libattr1/changelog.Debian.gz.old" 2> /dev/null
+	fi
+
+	# install basic build packages
+	# Additional suggested packages added per: https://wiki.debian.org/Python/LibraryStyleGuide
+	sudo apt-get install -y --force-yes build-essential bc debhelper \
+	python-pip python-psutil groff git python-setuptools dh-python \
+	python-all python-setuptools python-pip python-docutils python-sphinx
+
+	# These packages are now "Debianized" so we don't have to rely on hacky installs with pip
+	# python-addirs is an upsteam Debain package
+	# For the rest, see the respective github repositories under my profile
+	sudo apt-get install -y --force-yes python-appdirs python-pastebin python-psutil pysteam
+
+}
+
+main()
+{
+
+	# create BUILD_TMP
+	if [[ -d "${BUILD_TMP}" ]]; then
+
+		sudo rm -rf "${BUILD_TMP}"
+		mkdir -p "${BUILD_TMP}"
+
+	else
+
+		mkdir -p "${BUILD_TMP}"
+
+	fi
+
+	# enter build dir
+	cd "${BUILD_TMP}" || exit
+
+	# install prereqs for build
+	
+	if [[ "${BUILDER}" != "pdebuild" && "${BUILDER}" != "sbuild" ]]; then
+
+		# handle prereqs on host machine
+		install_prereqs
+
+	fi
+
+
+	echo -e "\n==> Obtaining upstream source code\n"
+
+	# clone and checkout desired commit
+	git clone -b "$rel_TARGET" "$GIT_URL" "${GIT_DIR}"
+
+	# Add debian folder
+	cp -r ""$SCRIPTDIR/debian"" "${GIT_DIR}"
+
+	# inject iur modified files
+	cp "$SCRIPTDIR/emulators.txt" "${GIT_DIR}"
+	cp "$SCRIPTDIR/config.txt" "${GIT_DIR}"
+	cp "$SCRIPTDIR/consoles.txt" "${GIT_DIR}"
+	cp "$SCRIPTDIR/ice-steamos.sh" "${GIT_DIR}/ice-steamos"
+	cp "$SCRIPTDIR/debian/README.md" "${GIT_DIR}"
+
+	#################################################
+	# Build package
+	#################################################
+
+	echo -e "\n==> Creating original tarball\n"
+	sleep 2s
+
+	# create the tarball from latest tarball creation script
+	# use latest revision designated at the top of this script
+
+	# Trim .git folders
+	find "${GIT_DIR}" -name "*.git" -type d -exec sudo rm -r {} \;
+
+	# create source tarball
+	tar -cvzf "${PKGNAME}_${PKGVER}+${PKGSUFFIX}.orig.tar.gz" "$PKGNAME"
+
+	# Enter git dir to build
+	cd "${GIT_DIR}"
+
+
+	echo -e "\n==> Updating changelog"
+	sleep 2s
+
+ 	# update changelog with dch
+	if [[ -f "debian/changelog" ]]; then
+
+		dch -p --force-distribution -v "${PKGVER}+${PKGSUFFIX}" --package "${PKGNAME}" -D "${DIST}" -u "${URGENCY}"
+
+	else
+
+		dch -p --create --force-distribution -v "${PKGVER}+${PKGSUFFIX}" --package "${PKGNAME}" -D "${DIST}" -u "${URGENCY}"
+
+	fi
+
+
+	#################################################
+	# Build Debian package
+	#################################################
+
+	echo -e "\n==> Building Debian package ${PKGNAME} from source\n"
+	sleep 2s
+
+	DIST=$DIST ARCH=$ARCH ${BUILDER} ${BUILDOPTS}
+
+	#################################################
+	# Cleanup
+	#################################################
+
+	# clean up dirs
+
+	# note time ended
+	time_end=$(date +%s)
+	time_stamp_end=(`date +"%T"`)
+	runtime=$(echo "scale=2; ($time_end-$TIME_START) / 60 " | bc)
+
+	# output finish
+	echo -e "\nTime started: ${TIME_STAMP_START}"
+	echo -e "Time started: ${time_stamp_end}"
+	echo -e "Total Runtime (minutes): $runtime\n"
+
+	# assign value to build folder for exit warning below
+	build_folder=$(ls -l | grep "^d" | cut -d ' ' -f12)
+
+	# back out of build tmp to script dir if called from git clone
+	if [[ "${SCRIPTDIR}" != "" ]]; then
+		cd "${SCRIPTDIR}" || exit
+	else
+		cd "${HOME}" || exit
+	fi
+
+	# inform user of packages
+	cat<<- EOF
+	#################################################################
+	If package was built without errors you will see it below.
+	If you don't, please check build dependency errors listed above.
+	#################################################################
+
+	EOF
+
+	echo -e "Showing contents of: ${BUILD_TMP}: \n"
+	ls "${BUILD_TMP}" | grep -E *${PKGVER}*
+
+	# Ask to transfer files if debian binries are built
+	# Exit out with log link to reivew if things fail.
+
+	if [[ $(ls "${BUILD_TMP}" | grep -w "deb" | wc -l) -gt 0 ]]; then
+
+		echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
+		sleep 0.5s
+		# capture command
+		read -erp "Choice: " transfer_choice
+
+		if [[ "$transfer_choice" == "y" ]]; then
+
+			# copy files to remote server
+			rsync -arv --info=progress2 -e "ssh -p ${REMOTE_PORT}" \
+			--filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" \
+			${BUILD_TMP}/ ${REMOTE_USER}@${REMOTE_HOST}:${REPO_FOLDER}
+
+			# uplaod local repo changelog
+			cp "${GIT_DIR}/debian/changelog" "${SCRIPTDIR}/debian"
+
+		elif [[ "$transfer_choice" == "n" ]]; then
+			echo -e "Upload not requested\n"
+		fi
+
+	else
+
+		# Output log file to sprunge (pastebin) for review
+		echo -e "\n==OH NO!==\nIt appears the build has failed. See below log file:"
+		cat ${BUILD_TMP}/${PKGNAME}*.build | curl -F 'sprunge=<-' http://sprunge.us
+
+	fi
+
+}
+
+# start main
+main
+

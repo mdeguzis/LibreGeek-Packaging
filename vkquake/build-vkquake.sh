273c71dd2ee5cb62f3561c5eb1d2e85c43f133a6
professorkaos64 <mdeguzis@gmail.com>
2016-09-07 15:20:17 -0400

    update build scripts

--- vkquake/build-vkquake.sh
+++ vkquake/build-vkquake.sh
@@ -48,7 +48,7 @@ fi
 #GIT_URL="https://github.com/ProfessorKaos64/vkQuake"
 GIT_URL="https://github.com/Novum/vkQuake"
 #TARGET="master"
-TARGET="0.70"
+TARGET="0.71"
 
 # package vars
 DATE_LONG=$(date +"%a, %d %b %Y %H:%M:%S %z")
@@ -60,7 +60,7 @@ BUILDOPTS="--debbuildopts -nc"
 export STEAMOS_TOOLS_BETA_HOOK="true"
 PKGNAME="vkquake"
 # Source version from vkQuake/Quake/quakedef.h
-PKGVER="0.70.0"
+PKGVER="0.71.0"
 PKGREV="1"
 EPOCH="1"
 PKGSUFFIX="${DATE_SHORT}git+bsos"
--- vkquake/vkquake-static-binary/build-vkquake-static.sh
+++ vkquake/vkquake-static-binary/build-vkquake-static.sh
@@ -65,10 +65,10 @@ if [[ "$arg1" == "--testing" ]]; then
 
 else
 
-        TARGET="0.70"
+        TARGET="0.71"
         REPO_FOLDER="/home/mikeyd/packaging/linux-binaries/stable"
 	# Source version from vkQuake/Quake/quakedef.h
-	PKGVER="0.70.0"
+	PKGVER="0.71.0"
 
 fi
 

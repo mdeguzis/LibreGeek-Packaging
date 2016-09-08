aa9611dcb9470c68b2903d0675fe3da31968abff
professorkaos64 <mdeguzis@gmail.com>
2016-09-08 00:07:02 -0400

    add eduke32

---
+++ eduke32/build-eduke32.sh
@@ -0,0 +1,255 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+# Author:	Michael DeGuzis
+# Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt name:	build-eduke32.sh
+# Script Ver:	0.1.1
+# Description:	Attmpts to build a deb package from the latest eduke32 source
+#		code.
+#
+# See:		http://svn.eduke32.com/eduke32/polymer/
+#
+# Usage:	./build-eduke32.sh
+# Opts:		[--testing]
+#		Modifys build script to denote this is a test package build.
+# -------------------------------------------------------------------------------
+
+#################################################
+# Set variables
+#################################################
+
+arg1="$1"
+SCRIPTDIR=$(pwd)
+TIME_START=$(date +%s)
+TIME_STAMP_START=(`date +"%T"`)
+
+# Check if USER/HOST is setup under ~/.bashrc, set to default if blank
+# This keeps the IP of the remote VPS out of the build script
+
+if [[ "${REMOTE_USER}" == "" || "${REMOTE_HOST}" == "" ]]; then
+
+	# fallback to local repo pool TARGET(s)
+	REMOTE_USER="mikeyd"
+	REMOTE_HOST="archboxmtd"
+	REMOTE_PORT="22"
+
+fi
+
+if [[ "$arg1" == "--testing" ]]; then
+
+	REPO_FOLDER="/home/mikeyd/packaging/steamos-tools/incoming_testing"
+
+else
+
+	REPO_FOLDER="/home/mikeyd/packaging/steamos-tools/incoming"
+
+fi
+# upstream vars
+SVN_URL="http://svn.eduke32.com/eduke32/polymer/"
+# REV chosen below from list
+# SVN_REV="r5010"
+
+# package vars
+DATE_LONG=$(date +"%a, %d %b %Y %H:%M:%S %z")
+DATE_SHORT=$(date +%Y%m%d)
+ARCH="amd64"
+BUILDER="pdebuild"
+BUILDOPTS="--debbuildopts -sa"
+PKGNAME="eduke32"
+PKGVER="1.0.0"
+PKGREV="1"
+# PKGSUFFIX set below
+DIST="brewmaster"
+URGENCY="low"
+UPLOADER="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
+MAINTAINER="ProfessorKaos64"
+
+# set build directories
+export BUILD_TMP="${HOME}/build-${PKGNAME}-tmp"
+SRC_DIR="${PKGNAME}-${PKGVER}"
+SVN_DIR="${BUILD_TMP}/${SRC_DIR}"
+
+install_prereqs()
+{
+	clear
+	echo -e "==> Installing prerequisites for building...\n"
+	sleep 2s
+	# install basic build packages
+	sudo apt-get install -y --force-yes build-essential subversion \
+	nasm libgl1-mesa-dev libglu1-mesa-dev libsdl1.2-dev \
+	libsdl-mixer1.2-dev libsdl2-dev libsdl2-mixer-dev \
+	flac libflac-dev libvorbis-dev libpng-dev \
+	libvpx-dev libgtk2.0-dev freepats
+
+}
+
+main()
+{
+
+	# create BUILD_TMP
+	if [[ -d "${BUILD_TMP}" ]]; then
+
+		sudo rm -rf "${BUILD_TMP}"
+		mkdir -p "${BUILD_TMP}"
+
+	else
+
+		mkdir -p "${BUILD_TMP}"
+
+	fi
+
+	# enter build dir
+	cd "${BUILD_TMP}" || exit
+
+	# install prereqs for build
+	if [[ "${BUILDER}" != "pdebuild" && "${BUILDER}" != "sbuild" ]]; then
+
+		# handle prereqs on host machine
+		install_prereqs
+
+	else
+
+	  # still need subversion
+	  sudo apt-get install -y --force-yes subversion
+
+	fi
+
+	#################################################
+	# prepare package
+	#################################################
+
+	echo -e "\n==> Obtaining upstream source code\n"
+
+	# checkout desired revision
+	svn checkout "${SVN_URL}" "${SVN_DIR}"
+
+	# Get desired revision
+	echo -e "\n==> Showing last 5 revisions\n"
+
+	cd  "${SVN_DIR}"
+	svn log | grep -e ^r[0-9] | cut -d " " -f 1 | head -n 5
+
+	echo -e "\n==> Use which revision?"
+	sleep 0.3s
+	read -erp "Choice: " SVN_REV
+	echo ""
+	svn update "${SVN_REV}"
+
+	# Set package suffix
+	PKGSUFFIX="${SVN_REV}svn+bsos"
+
+	# Add extras
+	cp "${SCRIPTDIR}/eduke32.png" "${SVN_DIR}"
+	cp "${SCRIPTDIR}/quake-icon.png" "${SVN_DIR}"
+
+	#################################################
+	# Build package
+	#################################################
+
+	echo -e "\n==> Creating original tarball\n"
+	sleep 2s
+
+	# Trim .git folders
+	find "${GIT_DIR}" -name "*.git" -type d -exec sudo rm -r {} \;
+
+	# create source tarball
+	cd "${BUILD_TMP}" || exit
+	tar -cvzf "${PKGNAME}_${PKGVER}+${PKGSUFFIX}.orig.tar.gz" "${SRC_DIR}"
+
+	# Add required debian files
+	cp -r "${SCRIPTDIR}/debian" "${SVN_DIR}"
+
+	# enter source dir
+	cd "${SVN_DIR}"
+
+	echo -e "\n==> Updating changelog"
+	sleep 2s
+
+	# update changelog with dch
+	if [[ -f "debian/changelog" ]]; then
+
+		dch -p --force-distribution -v "${PKGVER}+${PKGSUFFIX}-${PKGREV}" --package "${PKGNAME}" \
+		-D "${DIST}" -u "${URGENCY}" "Update to SVN revision ${SVN_REV}"
+		nano "debian/changelog"
+
+	else
+
+		dch -p --create --force-distribution -v "${PKGVER}+${PKGSUFFIX}-${PKGREV}" \
+		--package "${PKGNAME}" -D "${DIST}" -u "${URGENCY}" "Initial build, revision ${LATEST_REV}"
+		nano "debian/changelog"
+
+	fi
+
+	#################################################
+	# Build Debian package
+	#################################################
+
+	echo -e "\n==> Building Debian package ${PKGNAME} from source\n"
+	sleep 2s
+
+	USENETWORK=$NETWORK DIST=$DIST ARCH=$ARCH ${BUILDER} ${BUILDOPTS}
+
+	#################################################
+	# Cleanup
+	#################################################
+
+	# clean up dirs
+
+	# note time ended
+	time_end=$(date +%s)
+	time_stamp_end=(`date +"%T"`)
+	runtime=$(echo "scale=2; ($time_end-$TIME_START) / 60 " | bc)
+
+	# output finish
+	echo -e "\nTime started: ${TIME_STAMP_START}"
+	echo -e "Time started: ${time_stamp_end}"
+	echo -e "Total Runtime (minutes): $runtime\n"
+	# inform user of packages
+	cat<<- EOF
+	#################################################################
+	If package was built without errors you will see it below.
+	If you don't, please check build dependency errors listed above.
+	#################################################################
+
+	EOF
+
+	echo -e "Showing contents of: ${BUILD_TMP}: \n"
+	ls "${BUILD_TMP}" | grep -E *${PKGVER}*
+
+	# Ask to transfer files if debian binries are built
+	# Exit out with log link to reivew if things fail.
+
+	if [[ $(ls "${BUILD_TMP}" | grep -w "deb" | wc -l) -gt 0 ]]; then
+
+		echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
+		sleep 0.5s
+		# capture command
+		read -erp "Choice: " transfer_choice
+
+		if [[ "$transfer_choice" == "y" ]]; then
+
+			# copy files to remote server
+			rsync -arv --info=progress2 -e "ssh -p ${REMOTE_PORT}" \
+			--filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" \
+			${BUILD_TMP}/ ${REMOTE_USER}@${REMOTE_HOST}:${REPO_FOLDER}
+
+			# uplaod local repo changelog
+			cp "${SVN_DIR}/debian/changelog" "${SCRIPTDIR}/debian"
+
+		elif [[ "$transfer_choice" == "n" ]]; then
+			echo -e "Upload not requested\n"
+		fi
+
+	else
+
+		# Output log file to sprunge (pastebin) for review
+		echo -e "\n==OH NO!==\nIt appears the build has failed. See below log file:"
+		cat ${BUILD_TMP}/${PKGNAME}*.build | curl -F 'sprunge=<-' http://sprunge.us
+
+	fi
+
+}
+
+# start main
+main
+

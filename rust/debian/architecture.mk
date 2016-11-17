5742baa1c41ced02771fe7eb9ac555eaefe7a5be
ProfessorKaos64 <mdeguzis@gmail.com>
2016-11-17 03:37:00 +0000

    update rust/carge, fix retroarch cores

--- rust/debian/architecture.mk
+++
@@ -1,9 +0,0 @@
-# This Makefile snippet defines DEB_*_RUST_TYPE triples based on DEB_*_GNU_TYPE
-
-include /usr/share/dpkg/architecture.mk
-
-rust_cpu = $(subst i586,i686,$(if $(findstring -armhf-,-$(2)-),$(subst arm,armv7,$(1)),$(1)))
-rust_type_setvar = $(1)_RUST_TYPE ?= $(call rust_cpu,$($(1)_GNU_CPU),$($(1)_ARCH))-unknown-$($(1)_GNU_SYSTEM)
-
-$(foreach machine,BUILD HOST TARGET,\
-  $(eval $(call rust_type_setvar,DEB_$(machine))))

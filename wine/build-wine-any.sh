29dc31ceb8b8ea24c91234786702a36507d9dabe
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-11-19 13:53:55 -0500

    Update build-wine-any.sh

--- wine/build-wine-any.sh
+++ wine/build-wine-any.sh
@@ -138,35 +138,7 @@ install_prereqs()
 	case $OS in
 
 		Arch)
-			PKGS="
-			giflib                lib32-giflib
-			libpng                lib32-libpng
-			gnutls                lib32-gnutls
-			libxinerama           lib32-libxinerama
-			libxcomposite         lib32-libxcomposite
-			libxmu                lib32-libxmu
-			libxxf86vm            lib32-libxxf86vm
-			libldap               lib32-libldap
-			mpg123                lib32-mpg123
-			openal                lib32-openal
-			v4l-utils             lib32-v4l-utils
-			libpulse              lib32-libpulse
-			alsa-lib              lib32-alsa-lib
-			libxcomposite         lib32-libxcomposite
-			mesa                  lib32-mesa
-			mesa-libgl            lib32-mesa-libgl
-			libcl                 lib32-libcl
-			libxslt               lib32-libxslt
-			gst-plugins-base-libs lib32-gst-plugins-base-libs
-			samba
-			opencl-headers"
-
-			for PKG in ${PKGS}; 
-			do
-
-				sudo pacman -S --noconfirm ${PKG}
-
-			done
+		:
 
 		;;
 

82b04124bb4f781ceeec6bb7c060352da8624b9d
ProfessorKaos64 <ProfessorKaos64@users.noreply.github.com>
2016-11-22 08:28:16 -0500

    Update build-test-fedora-chroot.sh

--- setup-files/build-test-fedora-chroot.sh
+++ setup-files/build-test-fedora-chroot.sh
@@ -1,42 +1,237 @@
-#!/bin/bash
+#!/bin/bash 
 
-# Get utils
-sudo apt-get install -y yum
+# Creates base fedora image for Docker
+# Author: Michael DeGuzis
+# TODO: fix up code for arugment processing
+# Sourced from: https://github.com/ProfessorKaos64/docker/blob/fedora-32/create-fedora-docker.sh
 
-# Vars
-CHROOT_DEVEL=/chroot/devel/
-CHROOT_BASE="${CHROOT_DEVEL}/var/lib/"
+# Set main var defaults
 
-# 1 - Create the chroot directory
-sudo mkdir -p ${CHROOT_BASE}/rpm
+RELEASE="24"
+REVISION="3"
+ARCH="x86_64"
+NAME="fedora-${ARCH}"
+IMAGE_NAME="Fedora-${RELEASE}-${ARCH}"
+BASE_URL="https://kojipkgs.fedoraproject.org/packages/fedora-repos"
+REPO_RPM="${BASE_URL}/${RELEASE}/${REVISION}/noarch/fedora-repos-${RELEASE}-${REVISION}.noarch.rpm"
+BUILD_SCRIPT="https://raw.githubusercontent.com/docker/docker/master/contrib/mkimage-yum.sh"
+BASE_GROUPS="Core"
+BASE_PKGS="base base-devel"
 
-# 2 - Initiate rpm db on chroot
-sudo rpm --root ${CHOOR_DEVEL} --initdb
 
-# 3 - download Fedora Release package.
-# If you do not want Fedora, download the correct *-release package and use it 
-# on step 4. There are examples on the references.
-yumdownloader --destdir=/tmp fedora-release
+usage() 
+{
 
-# 4 - Install downloaded Fedora release inside chroot
-sudo rpm --root ${CHOOR_DEVEL} -ivh /tmp/fedora-release*rpm
+	cat<<- EOF
 
-# If you want more than just bash, see 5a - OPTIONAL
-# 5 - Install bash and its dependencies on the jail.
-# For different versions of Fedora or to install other RPM based distros, it will be
-# necessary to configure the correct yum repositories outside the jail, so yum
-# can download the correct packages.
-sudo yum --installroot=${CHOOR_DEVEL} install bash
+	$(basename $0) [OPTIONS] <name>
+	OPTIONS:
+	-a "<ARCH>"    		Arhitecture to use
+				The default is x86_64.
+	-g "<GROUPS>"    	Added groups to install
+				The default is base, base-devel
+	-n "<name>"		Desired name. Use your repo name if uploading later
+				The default is fedora-${ARCH}.
+	-p "<PACKAGES>"    	Added packages to install
+				The default is base base-devel.
+	-r "<fedora_rel_num>"	Fedora release number to use
+				The default is 24.
 
-#5a - OPTIONAL: Do a minimum system install
-# If you want all packages from the minimum install option
-# `yum grouplist` show all available groups
-sudo yum --installroot=${CHOOR_DEVEL} groupinstall "minimal install"
+	EOF
 
-#Post-Notes:
+}
 
-#- To enter the jail:
-#	$ sudo chroot /chroot/devel
-#- If you want to install yum inside the jail:
-#	$ sudo yum --installroot=/chroot/devel install yum
+check_distro()
+(
 
+	# Check first if we are using Fedora or Debian etc.
+	# Distros, such as Debian, only have yum at the moment
+
+	if which lsb_release &> /dev/null; then
+
+		OS=$(lsb_release -si)
+
+	else
+
+		OS=$(cat /etc/os-release | grep -w "NAME" | cut -d'=' -f 2)
+
+	fi
+
+	if [[ "${OS}" == "Debian" ]]; then
+
+		PKG_HANDLER="yum"
+		PKG_CONF="/etc/yum/yum.conf"
+
+	if [[ "${OS}" == "Fedora" ]]; then
+
+		PKG_HANDLER="dnf"
+		PKG_CONF="/etc/dnf/dnf.conf"
+
+	else
+
+		# Assume only yum available
+		PKG_HANDLER="yum"
+		PKG_CONF="/etc/yum/yum.conf"
+
+	fi
+
+)
+
+push_image_to_docker()
+{
+
+	echo -e "\n==> Displaying docker image, please enter tag ID, username, and desired tag (default: latest)\nn"
+	sleep 2s
+
+	docker images | grep "${NAME}"
+	echo ""
+	
+	read -erp "Username: " DOCKER_ USERNAME
+	read -erp "Image ID: " IMAGE_ID
+
+	if [[ -z "${TAG}" ]]; then
+
+		TAG="latest"
+
+	fi
+
+	echo -e "\n==> Logging in and pushing image\n
+
+	# login and push image
+	docker login
+	docker tag "${IMAGE_ID}" ${DOCKER_ USERNAME}/${NAME}:${TAG}
+	docker push  ${DOCKER_ USERNAME}/${NAME}
+
+}
+
+build_image()
+{
+	if [[ -f "${TMP_DIR}" ]]; then
+
+		rm -rf "${TMP_DIR}"
+		mkdir -p "${TMP_DIR}"
+
+	else
+
+		mkdir -p "${TMP_DIR}"
+
+
+	fi
+
+	# Set conf location
+	TEMP_PKG_CONF="${TMP_DIR}/etc/${PKG_HANDLER}"
+
+	# Enter tmp dir
+
+	cd "${TMP_DIR}"
+
+	# Download required files
+
+	wget "${BUILD_SCRIPT}" -q -n --show-progress
+
+	# if this fails, use revision 1, whichi shoudl always exist
+
+	if ! wget "${REPO_RPM}" -q -nc --show-progress; then
+
+		echo -e "\nERROR: Cannot find this file, using revision 1\n"
+		REPO_RPM="${BASE_URL}/${RELEASE}/1/noarch/fedora-repos-${RELEASE}-1.noarch.rpm"
+		wget "${REPO_RPM}" -q -nc --show-progress
+
+	fi
+
+	chmod +x mkimage-yum.sh
+
+	# Extract and modify base source repos RPM
+	# See: http://www.cyberciti.biz/tips/how-to-extract-an-rpm-package-without-installing-it.html
+
+	rpm2cpio "${REPO_RPM}" | xz -d | cpio -idmv
+
+	# Proceed as long as etc exists
+
+	if [[ -d "etc" && -f "PKG_CONF" ]]; then
+
+		# copy PKG_CONF from system
+		# dnf still pulls from /etc/yum/yum.repos.d/ for extra configuration
+
+		mkdir -p "${TEMP_PKG_CONF}"
+		cp "PKG_CONF" "${TEMP_PKG_CONF}"
+		sed -i "s/\$releasever/${RELEASE}/g" ${TMP_DIR}/etc/yum.repos.d/*
+		sed -i "s/\$basearcg/${ARCH}/g" ${TMP_DIR}/etc/yum.repos.d/*
+
+		# Enable base repos
+		sed -i "s/\enabled\=0/enabled\=1}/g" "${TMP_DIR}/etc/yum.repos.d/fedora.repo"
+		sed -i "s/\enabled\=0/enabled\=1}/g" "${TMP_DIR}/etc/yum.repos.d/fedora-updates.repo"
+
+		# Disable GPG check for image build
+		sed -i "s/\gpgcheck\=1/gpgcheck\=0}/g" "${TMP_DIR}/etc/yum.repos.d/fedora.repo"
+		sed -i "s/\gpgcheck\=2/gpgcheck\=0}/g" "${TMP_DIR}/etc/yum.repos.d/fedora-updates.repo"
+
+		# Add the contents of the repo files to etc/dnf/dnf.conf
+		# mkimage-yum.sh only uses the base .conf file to build the repo information
+		find etc -name '*.repo' -exec cat {} >> "${TEMP_PKG_CONF}" \;
+
+
+	else
+
+		echo -e "\nERROR: Cannot find etc directory or PKG_CONF!"
+		exit 1
+	fi
+
+
+	# Build image
+	if ! sudo ./mkimage-yum.sh -p ${BASE_PKGS} -g ${BASE_GROUPS} -y ${TEMP_PKG_CONF} ${NAME}; then
+
+		echo -e "\nERROR: Failed to create image! Exiting"
+		exit 1
+
+	fi
+
+	# ask to push image
+	echo -e "\n==> Push image to docker repostiry? If the repository does not exist, it will be created\n"
+	read -erp "Choice (y/n): " DOCKER_PUSH
+
+	if [[ "${DOCKER_PUSH}" == "y" ]]; then	
+
+		push_image_to_docker
+
+	fi
+}
+
+# Set ARCH, REVISION and release and release defaults
+while getopts ":a:g:n:p:r" opt; do
+	case $opt in
+
+		r)
+	    	RELEASE=$OPTARG
+		;;
+
+		a)
+	    	ARCH=$OPTARG
+	    	;;
+
+		n)
+	    	NAME=$OPTARG
+	    	;;
+
+		g)
+	    	BASE_GROUPS="$OPTARG"
+		;;
+
+		p)
+	    	BASE_PKGS="$OPTARG"
+		;;
+
+		\?)
+		echo "Invalid option: -$OPTARG"
+		usage
+		;;
+
+	esac
+done
+shift $((OPTIND - 1))
+
+#############################
+# Start script
+#############################
+check_distro
+build_image

954d38e699e144542386024f11a18e1ad334f229
ProfessorKaos64 <mdeguzis@gmail.com>
2016-11-25 13:37:14 +0000

    start mastodon

--- mastodon/build-mastodon.sh
+++ mastodon/build-mastodon.sh
@@ -28,7 +28,7 @@ time_stamp_start=(`date +"%T"`)
 
 if [[ "${REMOTE_USER}" == "" || "${REMOTE_HOST}" == "" ]]; then
 
-	# fallback to local repo pool target(s)
+	# fallback to local repo pool TARGET(s)
 	REMOTE_USER="mikeyd"
 	REMOTE_HOST="archboxmtd"
 	REMOTE_PORT="22"
@@ -46,28 +46,27 @@ else
 fi
 
 # upstream vars
-git_url="https://github.com/Gargron/mastodon"
-target="v0.9"
+SRC_URL="https://github.com/Gargron/mastodon"
+TARGET="v0.9"
 
 # package vars
-date_long=$(date +"%a, %d %b %Y %H:%M:%S %z")
-date_short=$(date +%Y%m%d)
+DATE_LONG=$(date +"%a, %d %b %Y %H:%M:%S %z")
+DATE_SHORT=$(date +%Y%m%d)
 ARCH="amd64"
 BUILDER="pdebuild"
 BUILDOPTS="--debbuildopts -sa"
 export USE_NETWORK="yes"
-pkgname="mastodon"
-pkgver=$(echo ${target} | sed 's/v//g' )
-pkgrev="1"
-DIST="jessie"
-urgency="low"
+PKGNAME="mastodon"
+PKGVER=$(echo ${TARGET} | sed 's/v//g' )
+PKGREV="1"
+DIST="${DIST:=jessie}"
+URGENCY="low"
 uploader="SteamOS-Tools Signing Key <mdeguzis@gmail.com>"
 maintainer="ProfessorKaos64"
 
 # set BUILD_TMP
-export BUILD_TMP="${HOME}/build-${pkgname}-temp"
-src_dir="${pkgname}-${pkgver}"
-git_dir="${BUILD_TMP}/${src_dir}"
+export BUILD_TMP="${HOME}/build-${PKGNAME}-temp"
+SRC_DIR="${BUILD_TMP}/${PKGNAME}-${PKGVER}"
 
 install_prereqs()
 {
@@ -75,8 +74,8 @@ install_prereqs()
 	echo -e "==> Installing prerequisites for building...\n"
 	sleep 2s
 	# install basic build packages
-	sudo apt-get -y --force-yes install python dh-python libgtk-3-dev  python-all-dev \
-	python-gobject-2-dev python-pylibacl
+	sudo apt-get -y --force-yes install build-essential libpq-dev libxml2-dev \
+	libxslt1-dev nodejs ffmpeg
 
 }
 
@@ -107,14 +106,19 @@ main()
 
 	fi
 
-	# Clone upstream source code and target
+	# Clone upstream source code and TARGET
 
 	echo -e "\n==> Obtaining upstream source code\n"
 
 	# clone
-	git clone  -b "${target}" "${git_url}" "${git_dir}"
+	git clone  -b "${TARGET}" "${SRC_URL}" "${SRC_DIR}"
 
-	#################################################
+	# Set suffix based on revisions
+	cd "${SRC_DIR}"
+	LATEST_COMMIT=$(git log -n 1 --pretty=format:"%h")
+	PKGSUFFIX="git${DATE_SHORT}.${LATEST_COMMIT}"
+
+	###############################################
 	# Build package
 	#################################################
 
@@ -123,13 +127,13 @@ main()
 
 	# create source tarball
 	cd "${BUILD_TMP}"
-	tar -cvzf "${pkgname}_${pkgver}+${pkgsuffix}.orig.tar.gz" "${src_dir}"
+	tar -cvzf "${PKGNAME}_${PKGVER}+${PKGSUFFIX}.orig.tar.gz" $(basename ${SRC_DIR})
 
 	# Add debian dir
-	cp -r "${scriptdir}/debian" "${git_dir}"
+	cp -r "${scriptdir}/debian" "${SRC_DIR}"
 
 	# enter source dir
-	cd "${git_dir}"
+	cd "${SRC_DIR}"
 
 	echo -e "\n==> Updating changelog"
 	sleep 2s
@@ -137,15 +141,15 @@ main()
 	# Create basic changelog format if it does exist or update
 	if [[ -f "debian/changelog" ]]; then
 
-		dch -p --force-distribution -v "${pkgver}+${pkgsuffix}-${pkgrev}" \
-		--package "${pkgname}" -D $DIST -u "${urgency}" \
-		"Update release to ${pkgver}" && nano "debian/changelog"
+		dch -p --force-distribution -v "${PKGVER}+${PKGSUFFIX}-${PKGREV}" \
+		--package "${PKGNAME}" -D $DIST -u "${URGENCY}" \
+		"Update release to ${PKGVER}" && nano "debian/changelog"
 
 	else
 
-		dch -p --force-distribution --create -v "${pkgver}+${pkgsuffix}-${pkgrev}" \
-		--package "${pkgname}" -D "${DIST}" \
-		-u "${urgency}" "Initial upload attempt" && nano "debian/changelog"
+		dch -p --force-distribution --create -v "${PKGVER}+${PKGSUFFIX}-${PKGREV}" \
+		--package "${PKGNAME}" -D "${DIST}" \
+		-u "${URGENCY}" "Initial upload attempt" && nano "debian/changelog"
 
 	fi
 
@@ -153,7 +157,7 @@ main()
 	# Build Debian package
 	#################################################
 
-	echo -e "\n==> Building Debian package ${pkgname} from source\n"
+	echo -e "\n==> Building Debian package ${PKGNAME} from source\n"
 	sleep 2s
 
 	#  build
@@ -185,7 +189,7 @@ main()
 
 	EOF
 
-	ls "${BUILD_TMP}" | grep -E "${pkgver}" 
+	ls "${BUILD_TMP}" | grep -E "${PKGVER}" 
 
 	echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
 	sleep 0.5s
@@ -201,7 +205,7 @@ main()
 			${BUILD_TMP}/ ${REMOTE_USER}@${REMOTE_HOST}:${REPO_FOLDER}
 
 			# Preserve changelog
-			cp "${git_dir}/debian/changelog" "${scriptdir}/debian/changelog" 
+			cp "${SRC_DIR}/debian/changelog" "${scriptdir}/debian/changelog" 
 
 		fi
 

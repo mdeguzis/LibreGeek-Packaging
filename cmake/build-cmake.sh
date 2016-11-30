5970d3db2beb2f7efa56f7d3da57c372e1fd799e
ProfessorKaos64 <mdeguzis@gmail.com>
2016-11-30 12:37:21 +0000

    add cmake 3.6.x to jessie

---
+++ cmake/build-cmake.sh
@@ -0,0 +1,231 @@
+#!/bin/bash
+# -------------------------------------------------------------------------------
+# Author:    	  Michael DeGuzis
+# Git:	    	  https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name:	  build-cmake.sh
+# Script Ver:	  0.7.9
+# Description:	  Attmpts to build a deb package from Plex Media Player git source
+#                 Installs cmake to '/usr/local/bin/cmake'
+# See:		  https://cmake.org/download/
+# Usage:	 ./build-cmake.sh
+# Opts:		[--testing]
+#		Modifys build script to denote this is a test package build.
+# -------------------------------------------------------------------------------
+
+#################################################
+# Set variables
+#################################################
+
+arg1="$1"
+SCRIPTDIR=$(pwd)
+TIME_START=$(date +%s)
+TIME_STAMP_START=(`date +"%T"`)
+
+
+# Check if USER/HOST is setup under ~/.bashrc, set to default if blank
+# This keeps the IP of the remote VPS out of the build script
+
+if [[ "${REMOTE_USER}" == "" || "${REMOTE_HOST}" == "" ]]; then
+
+	# fallback to local repo pool TARGET(s)
+	REMOTE_USER="mikeyd"
+	REMOTE_HOST="archboxmtd"
+	REMOTE_PORT="22"
+
+fi
+
+
+
+if [[ "$arg1" == "--testing" ]]; then
+
+	REPO_FOLDER="/mnt/server_media_x/packaging/steamos-tools/incoming_testing"
+
+else
+
+	REPO_FOLDER="/mnt/server_media_x/packaging/steamos-tools/incoming"
+
+fi
+
+# upstream URL
+SRC_URL="https://cmake.org/cmake.git"
+TARGET="v3.6.1"
+
+# package vars
+DATE_LONG=$(date +"%a, %d %b %Y %H:%M:%S %z")
+DATE_SHORT=$(date +%Y%m%d)
+ARCH="amd64"
+BUILDER="pdebuild"
+BUILDOPTS="--debbuildopts -nc"
+export USE_NETWORK="yes"
+export STEAMOS_TOOLS_BETA_HOOK="false"
+export NO_LINTIAN="false"
+export NO_PKG_TEST="false"
+PKGNAME="cmake"
+UPLOADER="LibreGeek Signing Key <mdeguzis@gmail.com>"
+PKGVER="3.6.1"
+PKGREV="1"
+PKGSUFFIX="bpo8"
+DIST="${DIST:=jessie}"
+URGENCY="low"
+MAINTAINER="ProfessorKaos64"
+
+# build dirs
+export BUILD_TMP="$HOME/build-${PKGNAME}-tmp"
+SRC_DIR="${BUILD_TMP}/${PKGNAME}-${PKGVER}"
+
+install_prereqs()
+{
+	clear
+	echo -e "==> Installing prerequisites for building...\n"
+	sleep 2s
+	# install needed packages
+	sudo apt-get install -y --force-yes git devscripts build-essential checkinstall \
+	debhelper dpkg-dev libarchive-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev \
+	libjsoncpp-dev liblzma-dev libncurses5-dev procps python-sphinx qtbase5-dev zlib1g-dev
+
+}
+
+main()
+{
+
+
+	#################################################
+	# Fetch source
+	#################################################
+
+	# create and enter BUILD_TMP
+	if [[ -d "${BUILD_TMP}" ]]; then
+
+		sudo rm -rf "${BUILD_TMP}"
+		mkdir -p "${BUILD_TMP}"
+
+	else
+
+		mkdir -p "${BUILD_TMP}"
+
+	fi
+
+	# Enter build dir
+	cd "${BUILD_TMP}"
+
+	# install prereqs for build
+	if [[ "${BUILDER}" != "pdebuild" && "${BUILDER}" != "sbuild" ]]; then
+
+		# handle prereqs on host machine
+		install_prereqs
+
+	fi
+
+	echo -e "\n==> Fetching upstream source\n"
+
+	# Get upstream source
+	git clone -b "${TARGET}" "${SRC_URL}" "${SRC_DIR}"
+
+	#################################################
+	# Build cmake source
+	#################################################
+
+	echo -e "\n==> Creating original tarball\n"sleep 2s
+	sleep 2s
+
+	# Trim .git folders
+	find "${SRC_DIR}" -name "*.git" -type d -exec sudo rm -r {} \;
+
+	# create source tarball
+	cd "${BUILD_TMP}"
+	tar -cvzf "${PKGNAME}_${PKGVER}~${PKGSUFFIX}.orig.tar.gz" $(basename ${SRC_DIR})
+
+	# copy in debian folder
+	cp -r "${SCRIPTDIR}/debian" "${SRC_DIR}"
+
+	# enter source dir
+	cd "${SRC_DIR}"
+
+	# gather commits
+	commits_full=$(git log --pretty=format:"  * %cd %h %s")
+
+	echo -e "\n==> Updating changelog"
+	sleep 2s
+
+ 	# update changelog with dch
+	if [[ -f "debian/changelog" ]]; then
+
+		dch -p --force-distribution -v "${PKGVER}~${PKGSUFFIX}+${PKGREV}" --package "${PKGNAME}" \
+		-D "${DIST}" -u "${URGENCY}" "Update release"
+		nano "debian/changelog"
+
+	else
+
+		dch -p --create --force-distribution -v "${PKGVER}~${PKGSUFFIX}+${PKGREV}" --package "${PKGNAME}" \
+		-D "${DIST}" -u "${URGENCY}" "Initial upload"
+
+	fi
+
+	#################################################
+	# Build Debian package
+	#################################################
+
+	echo -e "\n==> Building Debian package ${PKGNAME} from source\n"
+	sleep 2s
+
+	#  build
+	DIST=$DIST ARCH=$ARCH ${BUILDER} ${BUILDOPTS}
+
+	#################################################
+	# Cleanup
+	#################################################
+
+	# note time ended
+	time_end=$(date +%s)
+	time_stamp_end=(`date +"%T"`)
+	runtime=$(echo "scale=2; ($time_end-$TIME_START) / 60 " | bc)
+
+	# inform user of packages
+	cat<<- EOF
+	#################################################################
+	If package was built without errors you will see it below.
+	If you don't, please check build dependency errors listed above.
+	#################################################################
+
+	EOF
+
+	echo -e "Showing contents of: ${BUILD_TMP}: \n"
+	ls "${BUILD_TMP}" | grep -E *${PKGVER}*
+
+	# Ask to transfer files if debian binries are built
+	# Exit out with log link to reivew if things fail.
+
+	if [[ $(ls "${BUILD_TMP}" | grep -w "deb" | wc -l) -gt 0 ]]; then
+
+		echo -e "\n==> Would you like to transfer any packages that were built? [y/n]"
+		sleep 0.5s
+		# capture command
+		read -erp "Choice: " transfer_choice
+
+		if [[ "$transfer_choice" == "y" ]]; then
+
+			# copy files to remote server
+			rsync -arv --info=progress2 -e "ssh -p ${REMOTE_PORT}" \
+			--filter="merge ${HOME}/.config/SteamOS-Tools/repo-filter.txt" \
+			${BUILD_TMP}/ ${REMOTE_USER}@${REMOTE_HOST}:${REPO_FOLDER}
+
+			# uplaod local repo changelog
+			cp "${SRC_DIR}/debian/changelog" "${SCRIPTDIR}/debian"
+
+		elif [[ "$transfer_choice" == "n" ]]; then
+			echo -e "Upload not requested\n"
+		fi
+
+	else
+
+		# Output log file to sprunge (pastebin) for review
+		echo -e "\n==OH NO!==\nIt appears the build has failed. See below log file:"
+		cat ${BUILD_TMP}/${PKGNAME}*.build | curl -F 'sprunge=<-' http://sprunge.us
+
+	fi
+
+}
+
+# start main
+main
+

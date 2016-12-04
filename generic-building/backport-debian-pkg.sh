af5bc47177ad3370211762eea71106c29758f9d3
Michael T. DeGuzis <mtdeguzis@users.noreply.github.com>
2016-12-04 16:32:46 -0500

    need to unset build tmp before beginning

--- generic-building/backport-debian-pkg.sh
+++ generic-building/backport-debian-pkg.sh
@@ -90,6 +90,7 @@ function_set_vars()
 	export OLD_PKGNAME="${PKGNAME}"
 
 	# now set the build dir for results
+	unset BUILD_TMP
 	export BUILD_TMP="${BUILD_TMP:-${HOME}/package-builds/build-${PKGNAME}-tmp}"
 
 	echo -e "\nPress ENTER to use last: ${OLD_PKGVER}"

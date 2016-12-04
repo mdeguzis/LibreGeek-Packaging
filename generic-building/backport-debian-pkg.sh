edc63759be7db8bf31e238af044967d986105a51
Michael T. DeGuzis <mtdeguzis@users.noreply.github.com>
2016-12-04 16:26:27 -0500

    adjust versioning

--- generic-building/backport-debian-pkg.sh
+++ generic-building/backport-debian-pkg.sh
@@ -281,22 +281,22 @@ function_get_source()
 
 		brewmaster)
 		DIST_CODE="bsos"
-		PKGSUFFIX="${PKGREV}~${DIST_CODE}+${BACKPORTREV}"
+		PKGSUFFIX="${PKGREV}~${DIST_CODE}"
 		;;
 
 		brewmaster-tesing)
 		DIST_CODE="bsos"
-		PKGSUFFIX="${PKGREV}~${DIST_CODE}+${BACKPORTREV}"
+		PKGSUFFIX="${PKGREV}~${DIST_CODE}"
 		;;
 
 		jessie)
 		DIST_CODE="bpo8"
-		PKGSUFFIX="${PKGREV}~${DIST_CODE}+${BACKPORTREV}"
+		PKGSUFFIX="${PKGREV}~${DIST_CODE}"
 		;;
 
 		jessie-backports)
 		DIST_CODE="bpo8"
-		PKGSUFFIX="${PKGREV}~${DIST_CODE}+${BACKPORTREV}"
+		PKGSUFFIX="${PKGREV}~${DIST_CODE}"
 		;;
 
                 trusty)

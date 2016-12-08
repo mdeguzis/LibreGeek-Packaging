4671ced648a7aad53abc91af3c88429b1f0a7f68
Michael T. DeGuzis <mdeguzis@users.noreply.github.com>
2016-12-08 13:39:40 -0500

    Update build-local.sh

--- doomrl/build-local.sh
+++ doomrl/build-local.sh
@@ -1,1 +1,49 @@
 #!/bin/bash
+
+#
+# Only intended for testing a local build
+#
+
+TMP_DIR="${HOME}/doomrl-local-build-test"
+mkdir -p "${TMP_DIR}"
+
+DOOM_RL_SRC="${TMP_DIR}/doomrl"
+VALKYRIE_SRC="${TMP_DIR}/fpcvalkyrie"
+
+# deps
+sudo apt-get install -y --force-yes build-essential fpc lua5.1 liblua5.1-0-dev fp-units-base
+
+# Enter tmp dir
+cd "${TMP_DIR}"
+
+# Get files 
+if [[ ! -d "${DOOM_RL_SRC}" ]]; then
+
+	git clone https://github.com/ChaosForge/doomrl "${DOOM_RL_SRC}"
+
+else
+
+	cd  "${DOOM_RL_SRC}"
+	git pull
+
+fi
+
+if [[ ! -d "${VALKYRIE_SRC" ]]; then
+
+	git clone https://github.com/ChaosForge/fpcvalkyrie "${VALKYRIE_SRC}"
+	
+else
+
+	cd  "${VALKYRIE_SRC}"
+	git pull
+
+fi
+
+# back out to main src dir
+cd  "${DOOM_RL_SRC}"
+
+# Clean files
+git clean -f
+
+# build
+lua makefile.lua lq

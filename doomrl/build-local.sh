9ed6e136d4e0e4ff16bfd46a4a42f3c4267b2e80
Michael T. DeGuzis <mdeguzis@users.noreply.github.com>
2016-12-08 13:43:06 -0500

    ambiguous redirect

--- doomrl/build-local.sh
+++ doomrl/build-local.sh
@@ -4,52 +4,57 @@
 # Only intended for testing a local build
 #
 
-TMP_DIR="${HOME}/doomrl-local-build-test"
-mkdir -p "${TMP_DIR}"
+main ()
+{
 
-DOOM_RL_SRC="${TMP_DIR}/doomrl"
-VALKYRIE_SRC="${TMP_DIR}/fpcvalkyrie"
+	TMP_DIR="${HOME}/doomrl-local-build-test"
+	mkdir -p "${TMP_DIR}"
 
-# deps
-sudo apt-get install -y --force-yes build-essential fpc lua5.1 liblua5.1-0-dev \
-fp-units-base curl
+	DOOM_RL_SRC="${TMP_DIR}/doomrl"
+	VALKYRIE_SRC="${TMP_DIR}/fpcvalkyrie"
 
-# Enter tmp dir
-cd "${TMP_DIR}"
+	# deps
+	sudo apt-get install -y --force-yes build-essential fpc lua5.1 liblua5.1-0-dev \
+	fp-units-base curl
 
-# Get files 
-if [[ ! -d "${DOOM_RL_SRC}" ]]; then
+	# Enter tmp dir
+	cd "${TMP_DIR}"
 
-	git clone https://github.com/ChaosForge/doomrl "${DOOM_RL_SRC}"
+	# Get files 
+	if [[ ! -d "${DOOM_RL_SRC}" ]]; then
 
-else
+		git clone https://github.com/ChaosForge/doomrl "${DOOM_RL_SRC}"
 
+	else
+
+		cd  "${DOOM_RL_SRC}"
+		git pull
+
+	fi
+
+	if [[ ! -d "${VALKYRIE_SRC}" ]]; then
+
+		git clone https://github.com/ChaosForge/fpcvalkyrie "${VALKYRIE_SRC}"
+
+	else
+
+		cd  "${VALKYRIE_SRC}"
+		git pull
+
+	fi
+
+	# back out to main src dir
 	cd  "${DOOM_RL_SRC}"
-	git pull
 
-fi
+	# Clean files
+	git clean -f
 
-if [[ ! -d "${VALKYRIE_SRC}" ]]; then
+	# build
+	lua makefile.lua lq
 
-	git clone https://github.com/ChaosForge/fpcvalkyrie "${VALKYRIE_SRC}"
-	
-else
+}
 
-	cd  "${VALKYRIE_SRC}"
-	git pull
-
-fi
-
-# back out to main src dir
-cd  "${DOOM_RL_SRC}"
-
-# Clean files
-git clean -f
-
-# build
-lua makefile.lua lq
-
-main 2>$1 | tee log.txt
+main 2>&1 | tee log.txt
 
 echo "Log: "
 cat log.txt | curl -F 'sprunge=<-' http://sprunge.us

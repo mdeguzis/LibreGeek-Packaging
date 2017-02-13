bdf470ec2db41de1a317cbd49011876ba4dea4dd
Michael T. DeGuzis <mdeguzis@users.noreply.github.com>
2017-02-12 19:58:35 -0500

    bin was renamed

--- openpht/debian/openpht.sh
+++ openpht/debian/openpht.sh
@@ -3,4 +3,4 @@ export XBMC_HOME=/opt/openpht/share/XBMC
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/openpht/bin/system/players/dvdplayer/
 #Use export AE_ENGINE=SOFT to disable pulse audio
 #export AE_ENGINE=SOFT
-/opt/openpht/bin/plexhometheater
+/opt/openpht/bin/openpht

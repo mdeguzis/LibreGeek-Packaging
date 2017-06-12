5ad7eb5d6fa4d9c90309a0c1350060e24168fef5
mdeguzis <mdeguzis@gmail.com>
2017-06-12 15:46:48 -0400

    update git checker

--- git-status-check.py
+++ git-status-check.py
@@ -41,42 +41,39 @@ else:
 print "Scanning: " + target_path
 
 for dname, subdir_list, file_list in os.walk(os.path.abspath(target_path)):
-	if dname.endswith(".git"):
-		# Format all the things
-		actions = []
-		output = []
-		parentdir = dname.replace('.git','')
-		gitdir = os.path.splitext(parentdir)[0]
-		gitdir_basename = os.path.basename(os.path.abspath(parentdir))
-		print sub_divider + "Checking repository: '" + gitdir_basename + "'" + sub_divider
-		os.chdir(parentdir)
-		print "Absolute path: " + parentdir
-		print "Git status: \n"
-		gitstatus = subprocess.Popen(['git', 'status'], stdout=subprocess.PIPE)
+	if args.skip:
+		for keyword in keywords:
+			if keyword not in dname and dname.endswith(".git"):
+				# Format all the things
+				actions = []
+				output = []
+				parentdir = dname.replace('.git','')
+				gitdir = os.path.splitext(parentdir)[0]
+				gitdir_basename = os.path.basename(os.path.abspath(parentdir))
+				print sub_divider + "Checking repository: '" + gitdir_basename + "'" + sub_divider
+				os.chdir(parentdir)
+				print "Absolute path: " + parentdir
+				print "Git status: \n"
+				gitstatus = subprocess.Popen(['git', 'status'], stdout=subprocess.PIPE)
 
-		# Loop the subprocess output
-		for line in gitstatus.stdout.readlines():
-			formatted_line = line.replace('\r', '').replace('\n', '')
-			print formatted_line
-			output.append(formatted_line)
+				# Loop the subprocess output
+				for line in gitstatus.stdout.readlines():
+					formatted_line = line.replace('\r', '').replace('\n', '')
+					print formatted_line
+					output.append(formatted_line)
 
-		# Check for anything that does not denote a clean working tree
-		if "working directory clean" not in str(output):
-			actions.append("\n\tWorking directory unclean")
-			status_actions[parentdir]= parentdir + ": " + ' '.join(actions)
+				# Check for anything that does not denote a clean working tree
+				if "working directory clean" not in str(output):
+					actions.append("\n\tWorking directory unclean")
+					status_actions[parentdir]= parentdir + ": " + ' '.join(actions)
 
-		# Check for staging branch use
-		if "master" not in str(output):
-			actions.append("\n\tNon-master branch in use, or detached state")
-			status_actions[parentdir]= parentdir + ' '.join(actions)
-			
+				# Check for staging branch use
+				if "master" not in str(output):
+					actions.append("\n\tNon-master branch in use, or detached state")
+					status_actions[parentdir]= parentdir + ' '.join(actions)
+					
 print sub_divider + "Repositories that require attention" + sub_divider
 print "Total: " + str(action_count) + "\n"
 
 for key, value in status_actions.iteritems():
-	if args.skip:
-		for keyword in keywords:
-			if keyword not in value:
-				print value
-	else:
-		print value
+	print value
